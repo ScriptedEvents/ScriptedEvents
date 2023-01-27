54f66444dad10913d29281a1ad67ea409886e6f5
Thunder <thundermaker300@gmail.com>
2023-01-27 16:21:44 -0500

    Add IF action

---
+++ ScriptedEvents/Handlers/DefaultActions/IfAction.cs
@@ -0,0 +1,31 @@
+﻿using ScriptedEvents.API.Features.Actions;
+using ScriptedEvents.API.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptedEvents.Actions
+{
+    public class IfAction : IAction
+    {
+        public string Name => "IF";
+
+        public string[] Aliases => Array.Empty<string>();
+
+        public string[] Arguments { get; set; }
+
+        public ActionResponse Execute()
+        {
+            ConditionResponse outcome = ConditionHelper.Evaluate(string.Join("", Arguments));
+            if (!outcome.Success)
+                return new(false, $"IF execution error: {outcome.Message}", ActionFlags.FatalError);
+
+            if (!outcome.Passed)
+                return new(true, flags: ActionFlags.StopEventExecution);
+
+            return new(true);
+        }
+    }
+}
