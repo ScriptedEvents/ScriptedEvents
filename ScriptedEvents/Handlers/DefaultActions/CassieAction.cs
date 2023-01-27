3444632263e1f83abaa3d46ff8a46e3e3e8e7e00
Thunder <thundermaker300@gmail.com>
2023-01-27 16:37:21 -0500

    CASSIE command enhancement

--- ScriptedEvents/Handlers/DefaultActions/CassieAction.cs
+++ ScriptedEvents/Handlers/DefaultActions/CassieAction.cs
@@ -17,11 +17,26 @@ namespace ScriptedEvents.Handlers.DefaultActions
             if (Arguments.Length < 1) return new(false, "Missing text!");
 
             string text = string.Join(" ", Arguments);
+
+            if (string.IsNullOrWhiteSpace(text))
+                return new(false, "Missing text!");
+
             string[] cassieArgs = text.Split('|');
             if (cassieArgs.Length == 1)
+            {
                 Cassie.MessageTranslated(text, text);
+            }
             else
-                Cassie.MessageTranslated(cassieArgs[0], cassieArgs[1]);
+            {
+                if (string.IsNullOrWhiteSpace(cassieArgs[0]))
+                    return new(false, "Cannot show captions without a corresponding message.");
+
+                if (string.IsNullOrWhiteSpace(cassieArgs[1]))
+                    Cassie.Message(cassieArgs[0]);
+                else
+                    Cassie.MessageTranslated(cassieArgs[0], cassieArgs[1]);
+            }
+
             return new(true, string.Empty);
         }
     }
