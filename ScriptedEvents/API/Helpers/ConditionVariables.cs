5f84e96318f731ac829e7cf2f06c816bb58cde51
Thunder <thundermaker300@gmail.com>
2023-01-27 13:30:16 -0500

    Add lots of new condition variables

--- ScriptedEvents/API/Helpers/ConditionVariables.cs
+++ ScriptedEvents/API/Helpers/ConditionVariables.cs
@@ -1,4 +1,7 @@
-﻿using Exiled.API.Features;
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using PlayerRoles;
+using System;
 using System.Linq;
 using UnityEngine;
 
@@ -27,15 +30,43 @@ namespace ScriptedEvents.API.Helpers
             .Replace("ROUNDSTARTED", Round.IsStarted)
             .Replace("!ROUNDSTARTED", !Round.IsStarted)
 
+            .Replace("WARHEADCOUNTING", Warhead.IsInProgress)
+            .Replace("!WARHEADCOUNTING", !Warhead.IsInProgress)
+
+            .Replace("SCP914ACTIVE", Exiled.API.Features.Scp914.IsWorking)
+            .Replace("!SCP914ACTIVE", !Exiled.API.Features.Scp914.IsWorking)
+
             .Replace("WARHEADDETONATED", Warhead.IsDetonated)
             .Replace("!WARHEADDETONATED", !Warhead.IsDetonated)
 
             // Floats
+            //-- CHANCE
+            .Replace("CHANCE", UnityEngine.Random.value)
+
+            //-- WORLD TIME
+            .Replace("DAYOFWEEK", DateTime.UtcNow.DayOfWeek)
+            .Replace("DAYOFMONTH", DateTime.UtcNow.Day)
+            .Replace("DAYOFYEAR", DateTime.UtcNow.DayOfYear)
+            .Replace("MONTH", DateTime.UtcNow.Month)
+            .Replace("YEAR", DateTime.UtcNow.Year)
+
+            //-- ROLE COUNT
+            .Replace("CDP", Player.Get(RoleTypeId.ClassD).Count())
+            .Replace("CHI", Player.Get(Team.ChaosInsurgency).Count())
+            .Replace("MTF", Player.Get(Team.FoundationForces).Count())
+            .Replace("SCPS", Player.Get(Side.Scp).Count())
+
+            //-- PLAYER COUNT
             .Replace("PLAYERSALIVE", Player.Get(ply => ply.IsAlive).Count())
             .Replace("PLAYERSDEAD", Player.Get(ply => ply.IsDead).Count())
-            .Replace("ROUNDSECONDS", Round.ElapsedTime.TotalSeconds)
+
+            //-- ROUND TIME
             .Replace("ROUNDMINUTES", Round.ElapsedTime.TotalMinutes)
-            .Replace("CHANCE", Random.value)
+            .Replace("ROUNDSECONDS", Round.ElapsedTime.TotalSeconds)
+
+            //-- TICKETS
+            .Replace("NTFTICKETS", Respawn.NtfTickets)
+            .Replace("CHAOSTICKETS", Respawn.ChaosTickets)
             ;
     }
 }
