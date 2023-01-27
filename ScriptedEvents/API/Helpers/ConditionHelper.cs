bd2f75d41af16a5cd870a3e675a746de7f6eee65
Thunder <thundermaker300@gmail.com>
2023-01-26 20:42:15 -0500

    Move some files

--- ScriptedEvents/API/Helpers/ConditionHelper.cs
+++ ScriptedEvents/API/Helpers/ConditionHelper.cs
@@ -9,7 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ScriptedEvents
+namespace ScriptedEvents.API.Helpers
 {
     public static class ConditionHelper
     {
--- ScriptedEvents/Handlers/DefaultActions/StopIfAction.cs
+++ ScriptedEvents/Handlers/DefaultActions/StopIfAction.cs
@@ -1,4 +1,5 @@
 ﻿using ScriptedEvents.API.Features.Actions;
+using ScriptedEvents.API.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
