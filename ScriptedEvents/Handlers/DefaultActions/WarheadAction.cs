8c5c8df68d6ecb5e870937cfcf1da72cac399d72
Thunder <thundermaker300@gmail.com>
2023-02-02 10:51:25 -0500

    Create WarheadAction.cs

---
+++ ScriptedEvents/Handlers/DefaultActions/WarheadAction.cs
@@ -0,0 +1,43 @@
+﻿using Exiled.API.Features;
+using ScriptedEvents.API.Features.Actions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptedEvents.Handlers.DefaultActions
+{
+    public class WarheadAction : IAction
+    {
+        public string Name => "WARHEAD";
+
+        public string[] Aliases => Array.Empty<string>();
+
+        public string[] Arguments { get; set; }
+
+        public ActionResponse Execute()
+        {
+            if (Arguments.Length < 1)
+                return new(false, "Missing argument: action (START/STOP/DETONATE/BLASTDOORS)");
+
+            switch (Arguments[0].ToUpper())
+            {
+                case "START":
+                    Warhead.Start();
+                    break;
+                case "STOP":
+                    Warhead.Stop();
+                    break;
+                case "DETONATE":
+                    Warhead.Detonate();
+                    break;
+                case "BLASTDOORS":
+                    Warhead.CloseBlastDoors();
+                    break;
+            }
+
+            return new(true);
+        }
+    }
+}
