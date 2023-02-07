89b133c96da176a5515448fddc1d1a05ea83db92
louis1706 <louismonneyron5@yahoo.com>
2023-02-07 20:39:36 +0100

    Rename Jump with GOTO

---
+++ ScriptedEvents/Handlers/DefaultActions/Goto.cs
@@ -0,0 +1,39 @@
+﻿using ScriptedEvents.API.Features;
+using ScriptedEvents.API.Features.Actions;
+using System;
+using System.Linq;
+
+namespace ScriptedEvents.Handlers.DefaultActions
+{
+    public class GotoAction : IScriptAction
+    {
+        public string Name => "GOTO";
+
+        public string[] Aliases => Array.Empty<string>();
+
+        public string[] Arguments { get; set; }
+
+        public ActionResponse Execute(Script script)
+        {
+            switch (Arguments.ElementAtOrDefault(0).ToUpper())
+            {
+                case "ADD":
+                    if (int.TryParse(Arguments.ElementAtOrDefault(1), out int newline))
+                    {
+                        script.CurrentLine += newline;
+                        return new(true);
+                    }
+
+                    break;
+                case "NUMBER":
+                    if (int.TryParse(Arguments.ElementAtOrDefault(1), out newline))
+                    {
+                        script.CurrentLine = newline;
+                        return new(true);
+                    }
+                    break;
+            }
+            return new(false);
+        }
+    }
+}
