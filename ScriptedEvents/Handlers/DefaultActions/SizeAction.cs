a10f030937c3af9a90bb048b30036b6bd62d9c51
louis1706 <louismonneyron5@yahoo.com>
2023-02-08 22:54:40 +0100

    Size

---
+++ ScriptedEvents/Handlers/DefaultActions/SizeAction.cs
@@ -0,0 +1,86 @@
+﻿using Exiled.API.Features;
+using PlayerRoles;
+using ScriptedEvents.API.Features.Actions;
+using ScriptedEvents.API.Helpers;
+using ScriptedEvents.Handlers.Variables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ScriptedEvents.Handlers.DefaultActions
+{
+    public class SizeAction : IScriptAction, IHelpInfo
+    {
+        public string Name => "SIZE";
+
+        public string[] Aliases => Array.Empty<string>();
+
+        public string[] Arguments { get; set; }
+
+        public string Description => "Sets all players to the given size.";
+
+        public Argument[] ExpectedArguments => new[]
+        {
+            new Argument("players", typeof(List<Player>), "The players to save as the new variable.", true),
+            new Argument("size X", typeof(float), "The X size to put on the player.", true),
+            new Argument("size Y", typeof(float), "The Y size to put on the player.", true),
+            new Argument("size Z", typeof(float), "The Z size to put on the player.", true),
+            new Argument("max", typeof(int), "The maximum amount of players to save in this variable (default: unlimited).", false),
+        };
+
+        public ActionResponse Execute(Script scr)
+        {
+            if (Arguments.Length < 4)
+            {
+                return new(false, "Missing arguments: players, size X, size Y, size Z, max(optional)");
+            }
+            if (float.TryParse(Arguments.ElementAt(1), out float x))
+                return new(false, $"Should be a float value {Arguments.ElementAt(1)}");
+            if (float.TryParse(Arguments.ElementAt(2), out float y))
+                return new(false, $"Should be a float value {Arguments.ElementAt(2)}");
+            if (float.TryParse(Arguments.ElementAt(3), out float z))
+                return new(false, $"Should be a float value {Arguments.ElementAt(3)}");
+
+            int max = -1;
+
+            if (Arguments.Length > 5)
+            {
+                string formula = ConditionVariables.ReplaceVariables(string.Join(" ", Arguments.Skip(2)));
+
+                try
+                {
+                    float maxFloat = (float)ConditionHelper.Math(formula);
+                    if (maxFloat != (int)maxFloat)
+                    {
+                        max = Mathf.RoundToInt(maxFloat);
+                    }
+                    else
+                    {
+                        max = (int)maxFloat;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return new(false, $"Invalid maximum condition provided! Condition: {formula} Error type: '{ex.GetType().Name}' Message: '{ex.Message}'.");
+                }
+
+                if (max < 0)
+                {
+                    return new(false, "A negative number cannot be used as the max argument of the SETROLE action.");
+                }
+            }
+
+
+            if (!ScriptHelper.TryGetPlayers(Arguments[0], max, out List<Player> plys))
+                return new(false, "No players matching the criteria were found.");
+
+            foreach (Player player in plys)
+                player.Scale = new(x, y, z);
+
+            return new(true);
+        }
+    }
+}
