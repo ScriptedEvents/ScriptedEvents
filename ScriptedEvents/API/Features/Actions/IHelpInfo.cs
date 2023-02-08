cbdaace34038c38dcdcc5c4c81bdb0fc06109c26
Thunder <thundermaker300@gmail.com>
2023-02-08 11:06:00 -0500

    Create HELP command

---
+++ ScriptedEvents/API/Features/Actions/IHelpInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptedEvents.API.Features.Actions
+{
+    public interface IHelpInfo
+    {
+        public string Description { get; }
+        public Argument[] ExpectedArguments { get; }
+    }
+
+    public class Argument
+    {
+        public Argument(string argumentName, string description, bool required)        {
+            ArgumentName = argumentName;
+            Description = description;
+            Required = required;
+        }
+
+        public string ArgumentName { get; }
+        public string Description { get; }
+        public bool Required { get; }
+    }
+}
---
+++ ScriptedEvents/Handlers/DefaultActions/HelpAction.cs
@@ -0,0 +1,52 @@
+﻿using Exiled.API.Features;
+using Exiled.API.Features.Pools;
+using ScriptedEvents.API.Features.Actions;
+using ScriptedEvents.API.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptedEvents.Handlers.DefaultActions
+{
+    public class HelpAction : IScriptAction, IHelpInfo
+    {
+        public string Name => "HELP";
+
+        public string[] Aliases => Array.Empty<string>();
+
+        public string[] Arguments { get; set; }
+
+        public string Description => "Gets information about a command.";
+
+        public Argument[] ExpectedArguments => new[] { new Argument("action", "The name of the action. Case-sensitive.", true) };
+
+        public ActionResponse Execute(Script script)
+        {
+            if (Arguments.Length < 1) return new(false, "Missing argument: Action name");
+
+            string actionString = Arguments[0];
+            if (!ScriptHelper.ActionTypes.TryGetValue(actionString, out Type type))
+                return new(false, $"Invalid action: {actionString}");
+
+            IAction action = Activator.CreateInstance(type) as IAction;
+
+            if (action is not IHelpInfo helpInfo)
+                return new(false, "The command provided is not supported in the HELP action.");
+
+            StringBuilder sb = StringBuilderPool.Pool.Get();
+
+            sb.AppendLine($"{action.Name}: {helpInfo.Description}\n\nArguments:\n");
+
+            foreach (var arg in helpInfo.ExpectedArguments)
+            {
+                sb.AppendLine($"{arg.ArgumentName}{(arg.Required ? "[R]" : string.Empty)}");
+                sb.AppendLine($"\t{arg.Description}");
+            }
+
+            Log.Info(StringBuilderPool.Pool.ToStringReturn(sb));
+            return new(true);
+        }
+    }
+}
