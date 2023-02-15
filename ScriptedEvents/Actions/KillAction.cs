f105728b34230ad44c28c0fc6224d717ba088219
Thunder <thundermaker300@gmail.com>
2023-02-15 08:57:11 -0500

    Add TpRoom and Kill

---
+++ ScriptedEvents/Actions/KillAction.cs
@@ -0,0 +1,57 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+    using Exiled.API.Enums;
+    using Exiled.API.Features;
+    using ScriptedEvents.Actions.Interfaces;
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Helpers;
+    using ScriptedEvents.Structures;
+
+    public class KillAction : IScriptAction, IHelpInfo
+    {
+        /// <inheritdoc/>
+        public string Name => "KILL";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public string Description => "Kills the targeted players.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+            new Argument("players", typeof(Player[]), "The players to kill.", true),
+            new Argument("damageType", typeof(string), "The DeathType to apply. Default: Unknown", false),
+        };
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script scr)
+        {
+            if (Arguments.Length < 1) return new(MessageType.InvalidUsage, this, null, (object)ExpectedArguments);
+
+            if (!ScriptHelper.TryGetPlayers(Arguments[0], null, out Player[] plys))
+                return new(MessageType.NoPlayersFound, this, "players");
+
+            if (Arguments.Length > 1)
+            {
+                if (!Enum.TryParse(Arguments[1], out DamageType damageType))
+                    return new(false, "Invalid DamageType provided.");
+
+                foreach (Player player in plys)
+                    player.Kill(damageType);
+
+                return new(true);
+            }
+
+            foreach (Player player in plys)
+                player.Kill(DamageType.Unknown);
+
+            return new(true);
+        }
+    }
+}
--- ScriptedEvents/Actions/TpRoomAction.cs
+++ ScriptedEvents/Actions/TpRoomAction.cs
@@ -1,6 +1,7 @@
 ﻿namespace ScriptedEvents.Actions
 {
     using System;
+    using Exiled.API.Enums;
     using Exiled.API.Features;
     using ScriptedEvents.Actions.Interfaces;
     using ScriptedEvents.API.Enums;
@@ -8,10 +9,10 @@
     using ScriptedEvents.Structures;
     using UnityEngine;
 
-    public class TpxAction : IScriptAction, IHelpInfo
+    public class TpRoomAction : IScriptAction, IHelpInfo
     {
         /// <inheritdoc/>
-        public string Name => "TPX";
+        public string Name => "TPROOM";
 
         /// <inheritdoc/>
         public string[] Aliases => Array.Empty<string>();
@@ -19,14 +20,12 @@
         /// <inheritdoc/>
         public string[] Arguments { get; set; }
 
-        public string Description => "Teleports players to the specified X, Y, Z coordinates.";
+        public string Description => "Teleports players to the specified room center.";
 
         public Argument[] ExpectedArguments => new[]
         {
             new Argument("players", typeof(Player[]), "The players to teleport", true),
-            new Argument("X", typeof(float), "The X-coordinate to teleport to.", true),
-            new Argument("Y", typeof(float), "The Y-coordinate to teleport to.", true),
-            new Argument("Z", typeof(float), "The Z-coordinate to teleport to.", true),
+            new Argument("room", typeof(RoomType), "The room to teleport to. Do NOT use Scp173 room!!!", true),
         };
 
         /// <inheritdoc/>
@@ -37,19 +36,13 @@
             if (!ScriptHelper.TryGetPlayers(Arguments[0], null, out Player[] players))
                 return new(MessageType.NoPlayersFound, this, "players");
 
-            if (!float.TryParse(Arguments[1], out float x))
-                return new(MessageType.NotANumber, this, "X", Arguments[1]);
-
-            if (!float.TryParse(Arguments[2], out float y))
-                return new(MessageType.NotANumber, this, "Y", Arguments[2]);
-
-            if (!float.TryParse(Arguments[3], out float z))
-                return new(MessageType.NotANumber, this, "Z", Arguments[3]);
+            if (!Enum.TryParse(Arguments[1], out RoomType rt))
+                return new(false, $"Invalid room: {Arguments[1]}");
 
             foreach (Player ply in players)
             {
                 if (ply.IsDead || !ply.IsConnected) continue;
-                ply.Teleport(new Vector3(x, y, z));
+                ply.Teleport(rt);
             }
 
             return new(true);
