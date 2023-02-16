17944a2f828958f8d78081208ca2021a3b7a0221
Thunder <thundermaker300@gmail.com>
2023-02-16 13:46:44 -0500

    Add DamageAction + Custom death messages for both

--- ScriptedEvents/Actions/DamageAction.cs
+++ ScriptedEvents/Actions/DamageAction.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Linq;
+    using Discord;
     using Exiled.API.Enums;
     using Exiled.API.Features;
     using ScriptedEvents.Actions.Interfaces;
@@ -9,10 +10,10 @@
     using ScriptedEvents.API.Helpers;
     using ScriptedEvents.Structures;
 
-    public class KillAction : IScriptAction, IHelpInfo
+    public class DamageAction : IScriptAction, IHelpInfo
     {
         /// <inheritdoc/>
-        public string Name => "KILL";
+        public string Name => "DAMAGE";
 
         /// <inheritdoc/>
         public string[] Aliases => Array.Empty<string>();
@@ -21,24 +22,28 @@
         public string[] Arguments { get; set; }
 
         /// <inheritdoc/>
-        public string Description => "Kills the targeted players.";
+        public string Description => "Damages the targeted player.";
 
         /// <inheritdoc/>
         public Argument[] ExpectedArguments => new[]
         {
             new Argument("players", typeof(Player[]), "The players to kill.", true),
-            new Argument("damageType", typeof(DamageType), "The DeathType to apply. Default: Unknown", false),
+            new Argument("damage", typeof(float), "The amount of damage to apply. Variables & Math are NOT supported.", true),
+            new Argument("damageType", typeof(string), "The DeathType to apply. If a DamageType is not matched, this will act as a custom message instead. Default: Unknown", false),
         };
 
         /// <inheritdoc/>
         public ActionResponse Execute(Script scr)
         {
-            if (Arguments.Length < 1) return new(MessageType.InvalidUsage, this, null, (object)ExpectedArguments);
+            if (Arguments.Length < 2) return new(MessageType.InvalidUsage, this, null, (object)ExpectedArguments);
 
             if (!ScriptHelper.TryGetPlayers(Arguments[0], null, out Player[] plys))
                 return new(MessageType.NoPlayersFound, this, "players");
 
-            if (Arguments.Length > 1)
+            if (!float.TryParse(Arguments[1], out float damage))
+                return new(MessageType.NotANumber, this, "damage", Arguments[1]);
+
+            if (Arguments.Length > 2)
             {
                 bool useDeathType = true;
                 string customDeath = null;
@@ -46,22 +51,22 @@
                 if (!Enum.TryParse(Arguments[2], true, out DamageType damageType))
                 {
                     useDeathType = false;
-                    customDeath = string.Join(" ", Arguments.Skip(1));
+                    customDeath = string.Join(" ", Arguments.Skip(2));
                 }
 
                 foreach (Player player in plys)
                 {
                     if (useDeathType)
-                        player.Kill(damageType);
+                        player.Hurt(damage, damageType);
                     else
-                        player.Kill(string.IsNullOrWhiteSpace(customDeath) ? "Unknown" : customDeath);
+                        player.Hurt(damage, string.IsNullOrWhiteSpace(customDeath) ? "Unknown" : customDeath);
                 }
 
                 return new(true);
             }
 
             foreach (Player player in plys)
-                player.Kill(DamageType.Unknown);
+                player.Hurt(damage, DamageType.Unknown);
 
             return new(true);
         }
