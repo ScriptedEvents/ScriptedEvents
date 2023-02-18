aa7b416114b16f5fa1b03a0026e9c0999fd7fb40
Thunder <thundermaker300@gmail.com>
2023-02-18 18:23:11 -0500

    Add better display for countdown

--- ScriptedEvents/API/Helpers/CountdownHelper.cs
+++ ScriptedEvents/API/Helpers/CountdownHelper.cs
@@ -21,6 +21,22 @@
             MainHandle = Timing.RunCoroutine(InternalCoroutine());
         }
 
+        public static string Display(TimeSpan time)
+        {
+            int seconds = Mathf.RoundToInt((float)time.TotalSeconds);
+
+            if (seconds <= 60)
+                return Mathf.RoundToInt((float)time.TotalSeconds).ToString();
+
+            string secondsStr = time.Seconds.ToString();
+            if (secondsStr.Length == 1)
+            {
+                secondsStr = $"0{secondsStr}";
+            }
+
+            return $"{time.Minutes}:{secondsStr}";
+        }
+
         private static IEnumerator<float> InternalCoroutine()
         {
             while (true)
@@ -36,7 +52,7 @@
                     if (!ct.Expired)
                     {
                         ply.ClearBroadcasts();
-                        ply.Broadcast(1, BroadcastString.Replace("{TEXT}", ct.Text).Replace("{TIME}", Mathf.RoundToInt((float)ct.TimeLeft.TotalSeconds)));
+                        ply.Broadcast(1, BroadcastString.Replace("{TEXT}", ct.Text).Replace("{TIME}", Display(ct.TimeLeft)));
                     }
                 }
 
@@ -55,7 +71,7 @@
 
             Countdowns.Add(player, ct);
             player.ClearBroadcasts();
-            player.Broadcast(1, BroadcastString.Replace("{TEXT}", ct.Text).Replace("{TIME}", Mathf.RoundToInt((float)ts.TotalSeconds)));
+            player.Broadcast(1, BroadcastString.Replace("{TEXT}", ct.Text).Replace("{TIME}", Display(ts)));
         }
     }
 }
