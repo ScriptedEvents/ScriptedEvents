0f7fed4005c96f47ba91cbe55c2311771e21db97
Thunder <thundermaker300@gmail.com>
2023-02-23 18:07:54 -0500

    Add CLEARCASSIE action

---
+++ ScriptedEvents/Actions/ClearCassieAction.cs
@@ -0,0 +1,34 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+    using Exiled.API.Features;
+    using ScriptedEvents.Actions.Interfaces;
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.Structures;
+    using ScriptedEvents.Variables.Handlers;
+
+    public class ClearCassieAction : IScriptAction, IHelpInfo
+    {
+        /// <inheritdoc/>
+        public string Name => "CLEARCASSIE";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public string Description => "Clears cassie queue.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => Array.Empty<Argument>();
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            Cassie.Clear();
+            return new(true);
+        }
+    }
+}
