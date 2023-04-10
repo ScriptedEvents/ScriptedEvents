50cc46c17746fdc719d09b6f637f82d217c82c9f
Thunder <thundermaker300@gmail.com>
2023-04-10 08:45:57 -0400

    Add {ENGAGEDGENERATORS}

---
+++ ScriptedEvents/Variables/Condition/MapVariables.cs
@@ -0,0 +1,36 @@
+﻿namespace ScriptedEvents.Variables.Condition
+{
+    using System.Linq;
+
+    using Exiled.API.Enums;
+    using Exiled.API.Features;
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.Variables.Interfaces;
+
+    public class MapVariables : IVariableGroup
+    {
+        /// <inheritdoc/>
+        public string GroupName => "Map";
+
+        /// <inheritdoc/>
+        public VariableGroupType GroupType => VariableGroupType.Condition;
+
+        /// <inheritdoc/>
+        public IVariable[] Variables { get; } = new[]
+        {
+            new EngagedGenerators(),
+        };
+    }
+
+    public class EngagedGenerators : IFloatVariable
+    {
+        /// <inheritdoc/>
+        public float Value => Generator.Get(GeneratorState.Engaged).Count();
+
+        /// <inheritdoc/>
+        public string Name => "{ENGAGEDGENERATORS}";
+
+        /// <inheritdoc/>
+        public string Description => "The amount of generators which are fully engaged.";
+    }
+}
