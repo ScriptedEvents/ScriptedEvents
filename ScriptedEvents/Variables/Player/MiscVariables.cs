76ba1c8bbe23eb0aecf4fe715274517532ac236a
Thunder <thundermaker300@gmail.com>
2023-04-09 21:24:27 -0400

    the

--- ScriptedEvents/Variables/Player/MiscVariables.cs
+++ ScriptedEvents/Variables/Player/MiscVariables.cs
@@ -29,7 +29,7 @@
         public string Name => "{INTERCOMSPEAKER}";
 
         /// <inheritdoc/>
-        public string Description => "Gets player who is speaking on the intercom.";
+        public string Description => "Gets the player who is speaking on the intercom.";
 
         /// <inheritdoc/>
         public IEnumerable<Player> Players => Player.Get(player => Intercom.Speaker == player);
