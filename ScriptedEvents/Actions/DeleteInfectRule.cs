dee8f15694e2f888bb559045e4e379f5c7b3daff
Thunder <thundermaker300@gmail.com>
2023-08-14 16:57:53 -0400

    Add DELINFECTRULE

---
+++ ScriptedEvents/Actions/DeleteInfectRule.cs
@@ -0,0 +1,45 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+    using PlayerRoles;
+    using ScriptedEvents.Actions.Interfaces;
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.Structures;
+
+    public class DeleleteInfectRule : IScriptAction, IHelpInfo
+    {
+        /// <inheritdoc/>
+        public string Name => "DELINFECTRULE";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.RoundRule;
+
+        /// <inheritdoc/>
+        public string Description => "Delete a currently-existing infection rule.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+            new Argument("role", typeof(RoleTypeId), "The role a player must die as to be infected.", true),
+        };
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            if (Arguments.Length < 3) return new(MessageType.InvalidUsage, this, null, (object)ExpectedArguments);
+
+            if (!Enum.TryParse(Arguments[0], true, out RoleTypeId oldRole))
+                return new(MessageType.InvalidRole, this, "role", Arguments[0]);
+
+            MainPlugin.Handlers.InfectionRules.RemoveAll(rule => rule.OldRole == oldRole);
+
+            return new(true);
+        }
+    }
+}
