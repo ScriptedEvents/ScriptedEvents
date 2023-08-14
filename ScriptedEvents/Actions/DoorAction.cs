e749b739c322d4489d4c57027b8730f10d0158c6
Thunder <thundermaker300@gmail.com>
2023-08-14 18:32:27 -0400

    Remove BROKEN DURATION CODE

--- ScriptedEvents/Actions/DoorAction.cs
+++ ScriptedEvents/Actions/DoorAction.cs
@@ -34,7 +34,6 @@
         {
             new Argument("mode", typeof(string), "The mode (LOCK, UNLOCK, OPEN, CLOSE, DESTROY).", true),
             new Argument("doors", typeof(List<Door>), "The doors to affect.", true),
-            new Argument("duration", typeof(float), "The duration. Leave blank for indefinite duration. Variables & Math are supported.", false),
         };
 
         /// <inheritdoc/>
@@ -45,47 +44,29 @@
             if (!ScriptHelper.TryGetDoors(Arguments[1], out Door[] doors))
                 return new(false, "Invalid door(s) provided!");
 
-            float duration = 0;
-            if (Arguments.Length > 2)
-            {
-                string formula = ConditionVariables.ReplaceVariables(string.Join(" ", Arguments.Skip(2)), script);
-
-                if (!ConditionHelper.TryMath(formula, out MathResult result))
-                {
-                    return new(MessageType.NotANumberOrCondition, this, "duration", formula, result);
-                }
-
-                if (result.Result < 0)
-                {
-                    return new(MessageType.LessThanZeroNumber, this, "duration", result.Result);
-                }
-
-                duration = result.Result;
-            }
-
             Action<Door> action;
-            Action<Door, float> revertAction;
             switch (Arguments[0].ToUpper())
             {
                 case "OPEN":
                     action = (door) => door.IsOpen = true;
-                    revertAction = RevertOpened;
                     break;
                 case "CLOSE":
                     action = (door) => door.IsOpen = false;
-                    revertAction = RevertOpened;
                     break;
                 case "LOCK":
-                    action = (door) => door.Lock(duration, DoorLockType.AdminCommand);
-                    revertAction = RevertLock;
+                    action = (door) =>
+                    {
+                        if (door.IsLocked)
+                            return;
+
+                        door.ChangeLock(DoorLockType.AdminCommand);
+                    };
                     break;
                 case "UNLOCK":
                     action = (door) => door.Unlock();
-                    revertAction = RevertLock;
                     break;
                 case "DESTROY":
                     action = (door) => door.BreakDoor();
-                    revertAction = null;
                     break;
                 default:
                     return new(MessageType.InvalidOption, this, "mode", Arguments[0], "OPEN/CLOSE/LOCK/UNLOCK/DESTROY");
@@ -93,25 +74,10 @@
 
             foreach (Door door in doors)
             {
-                if (duration != 0 && revertAction != null)
-                    revertAction(door, duration);
-
                 action(door);
             }
 
-            return new(true, string.Empty);
-        }
-
-        public void RevertOpened(Door door, float duration)
-        {
-            bool state = door.IsOpen;
-            Timing.CallDelayed(duration, () => door.IsOpen = state);
-        }
-
-        public void RevertLock(Door door, float duration)
-        {
-            DoorLockType state = door.DoorLockType;
-            Timing.CallDelayed(duration, () => door.ChangeLock(state));
+            return new(true);
         }
     }
 }
