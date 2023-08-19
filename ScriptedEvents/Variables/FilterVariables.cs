c775aeec858a66a8f4b938055b02b69578b2a126
Thunder <thundermaker300@gmail.com>
2023-08-18 23:15:26 -0400

    lets not be stupid now

--- ScriptedEvents/Variables/FilterVariables.cs
+++ ScriptedEvents/Variables/FilterVariables.cs
@@ -3,7 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-
+    using Exiled.API.Enums;
     using Exiled.API.Features;
     using PlayerRoles;
     using ScriptedEvents.Structures;
@@ -24,10 +24,10 @@
     public class RoleFilter : IFloatVariable, IPlayerVariable, IArgumentVariable
     {
         /// <inheritdoc/>
-        public string Name => "{ROLEFILTER}";
+        public string Name => "{FILTER}";
 
         /// <inheritdoc/>
-        public string Description => "Filters a player variable by a certain RoleType.";
+        public string Description => "Filters a player variable by a certain type.";
 
         /// <inheritdoc/>
         public string[] Arguments { get; set; }
@@ -36,7 +36,8 @@
         public Argument[] ExpectedArguments { get; } = new[]
         {
             new Argument("name", typeof(string), "The name of the variable to filter.", true),
-            new Argument("roleType", typeof(RoleTypeId), "The RoleTypeId to sort.", true),
+            new Argument("type", typeof(string), "The mode to use to filter. Valid modes: ROLE, ZONE", true),
+            new Argument("input", typeof(string), "What to use as the filter (RoleType, ZoneType, etc)", true),
         };
 
         /// <inheritdoc/>
@@ -47,15 +48,27 @@
         {
             get
             {
-                if (Arguments.Length < 2) return Enumerable.Empty<Player>();
-                if (!Enum.TryParse(Arguments[1], out RoleTypeId roleType)) return Enumerable.Empty<Player>();
+                // Todo: Throw error, not empty enumerable
+                if (Arguments.Length < 3) return Enumerable.Empty<Player>();
 
                 string name = Arguments[0].Replace("{", string.Empty).Replace("}", string.Empty);
 
                 var conditionVariable = VariableSystem.GetVariable($"{{{name}}}", null); // Todo support per script variables
                 if (conditionVariable.Item1 is not null && conditionVariable.Item1 is IPlayerVariable playerVariable)
                 {
-                    return playerVariable.Players.Where(ply => ply.Role.Type == roleType);
+                    switch (Arguments[1].ToString())
+                    {
+                        case "ROLE" when Enum.TryParse(Arguments[2], true, out RoleTypeId rt):
+                            return Player.List.Where(plr => plr.Role.Type == rt);
+                        case "TEAM" when Enum.TryParse(Arguments[2], true, out Team team):
+                            return Player.List.Where(plr => plr.Role.Team == team);
+                        case "ZONE" when Enum.TryParse(Arguments[2], true, out ZoneType zt):
+                            return Player.List.Where(plr => plr.Zone.HasFlag(zt));
+                        case "ROOM" when Enum.TryParse(Arguments[2], true, out RoomType room):
+                            return Player.List.Where(plr => plr.CurrentRoom.Type == room);
+                        default:
+                            return Enumerable.Empty<Player>();
+                    }
                 }
 
                 return Enumerable.Empty<Player>();
