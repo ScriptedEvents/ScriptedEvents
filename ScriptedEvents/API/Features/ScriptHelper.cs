bcbaa6cd4dd5d520c6d9b751d7f570ec09b37583
ZigyWasTaken <imanyarandi222@gmail.com>
2023-10-10 16:45:48 +0330

    Add new custom file extension .sel
    Me bad at github

--- ScriptedEvents/API/Features/ScriptHelper.cs
+++ ScriptedEvents/API/Features/ScriptHelper.cs
@@ -424,6 +424,15 @@ namespace ScriptedEvents.API.Features
                 fileDirectory = mainFolderFile;
                 text = File.ReadAllText(mainFolderFile);
             }
+            else
+            {
+                mainFolderFile = Path.Combine(ScriptPath, scriptName + ".sel");
+                if (File.Exists(mainFolderFile))
+                {
+                    fileDirectory = mainFolderFile;
+                    text = File.ReadAllText(mainFolderFile);
+                }
+            }
 
             foreach (string directory in Directory.GetDirectories(ScriptPath))
             {
@@ -432,6 +441,15 @@ namespace ScriptedEvents.API.Features
                 {
                     fileDirectory = fileName;
                     text = File.ReadAllText(fileName);
+                }
+                else
+                {
+                    fileName = Path.Combine(directory, scriptName + ".sel");
+                    if (File.Exists(fileName))
+                    {
+                        fileDirectory = fileName;
+                        text = File.ReadAllText(fileName);
+                    }
                 }
             }
 
