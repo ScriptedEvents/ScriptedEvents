bcaf1db45f1a4e3959292e5865d8a2afe1b7eec4
Thunder <thundermaker300@gmail.com>
2023-11-06 18:22:43 -0500

    Add SCPATTACK

--- ScriptedEvents/EventHandlers.cs
+++ ScriptedEvents/EventHandlers.cs
@@ -422,7 +422,8 @@
                 (ev.Attacker.Role.Type is RoleTypeId.Scp096 && DisabledKeys.Contains("SCP096ATTACK")) ||
                 (ev.Attacker.Role.Type is RoleTypeId.Scp173 && DisabledKeys.Contains("SCP173ATTACK")) ||
                 (ev.Attacker.Role.Type is RoleTypeId.Scp939 && DisabledKeys.Contains("SCP939ATTACK")) ||
-                (ev.Attacker.Role.Type is RoleTypeId.Scp3114 && DisabledKeys.Contains("SCP3114ATTACK")))
+                (ev.Attacker.Role.Type is RoleTypeId.Scp3114 && DisabledKeys.Contains("SCP3114ATTACK")) ||
+                (ev.Attacker.Role.Team is Team.SCPs && DisabledKeys.Contains("SCPATTACK")))
                 ev.IsAllowed = false;
 
             if (ev.Attacker is null || ev.Player is null || ev.Attacker == Server.Host)
