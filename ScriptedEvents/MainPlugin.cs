be7916962b1297ac52f9fde04dc104c39ba28afc
Thunder <thundermaker300@gmail.com>
2024-01-04 20:42:55 -0500

    Version bump

--- ScriptedEvents/MainPlugin.cs
+++ ScriptedEvents/MainPlugin.cs
@@ -93,7 +93,7 @@
         public override string Author => "Thunder + Johnodon";
 
         /// <inheritdoc/>
-        public override Version Version => new(2, 5, 2, 1);
+        public override Version Version => new(2, 6, 0);
 
         /// <inheritdoc/>
         public override Version RequiredExiledVersion => new(8, 4, 4);
