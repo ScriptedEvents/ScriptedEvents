c2ed240ec3c61d80b9cdc8710f0896d5c6459f2c
Thunder <thundermaker300@gmail.com>
2024-01-09 18:45:46 -0500

    Improve code of Round variable

--- ScriptedEvents/Variables/MiscVariables.cs
+++ ScriptedEvents/Variables/MiscVariables.cs
@@ -4,78 +4,64 @@
     using ScriptedEvents.API.Features;
     using ScriptedEvents.Structures;
     using ScriptedEvents.Variables.Interfaces;
+    using UnityEngine;
 
     public class MiscVariables : IVariableGroup
     {
         /// <inheritdoc/>
         public string GroupName => "Miscellaneous";
 
-
         /// <inheritdoc/>
         public IVariable[] Variables { get; } = new IVariable[]
         {
             new Round(),
         };
+    }
 
-        public class Round : IFloatVariable, IArgumentVariable, INeedSourceVariable
+    public class Round : IFloatVariable, IArgumentVariable, INeedSourceVariable
+    {
+        /// <inheritdoc/>
+        public string Name => "{ROUND}";
+
+        /// <inheritdoc/>
+        public string Description => "Returns a rounded version of a variable.";
+
+        /// <inheritdoc/>
+        public string[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public Script Source { get; set; }
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
         {
-            /// <inheritdoc/>
-            public string Name => "{ROUND}";
-
-            /// <inheritdoc/>
-            public string Description => "Returns a rounded version of a variable.";
-
-            /// <inheritdoc/>
-            public string[] Arguments { get; set; }
-
-            /// <inheritdoc/>
-            public Script Source { get; set; }
-
-            /// <inheritdoc/>
-            public Argument[] ExpectedArguments => new[]
-            {
                 new Argument("variable", typeof(string), "The name of the variable to round. Requires the variable to be a number.", true),
                 new Argument("mode", typeof(string), "Way of rounding the variable, either UP or DOWN.", true),
-            };
+        };
 
-            /// <inheritdoc/>
-            public float Value
+        /// <inheritdoc/>
+        public float Value
+        {
+            get
             {
-                get
+                if (Arguments.Length < 1)
                 {
-                    if (Arguments.Length < 1)
-                    {
-                        throw new ArgumentException(MsgGen.VariableArgCount(Name, new[] { "variable" }));
-                    }
-
-                    if (!VariableSystem.TryGetVariable(Arguments[0], out IConditionVariable variable, out _, Source, false))
-                    {
-                        throw new ArgumentException(MsgGen.VariableArgCount(Name, new[] { "invalid variable" }));
-                    }
-
-                    if (variable is not IStringVariable value)
-                    {
-                        throw new ArgumentException("yes");
-                    }
-
-                    string mode = Arguments.Length < 2 ? "UP" : Arguments[1];
-                    float roundedValue = 0;
-
-                    if (mode == "UP")
-                    {
-                        roundedValue = (float)Math.Ceiling(float.Parse(value.Value));
-                    }
-                    else if (mode == "DOWN")
-                    {
-                        roundedValue = (float)Math.Floor(float.Parse(value.Value));
-                    }
-                    else
-                    {
-                        throw new ArgumentException(MsgGen.VariableArgCount(Name, new[] { "mode is neither \"UP\" nor \"DOWN\"." }));
-                    }
-
-                    return roundedValue;
+                    throw new ArgumentException(MsgGen.VariableArgCount(Name, new[] { "variable" }));
                 }
+
+                if (!VariableSystem.TryParse(Arguments[0], out float value))
+                {
+                    throw new ArgumentException($"The provided value '{Arguments[0]}' is not a valid number or variable containing a number. [Error Code: SE-134]");
+                }
+
+                string mode = Arguments.Length < 2 ? "UP" : Arguments[1];
+
+                return mode.ToUpper() switch
+                {
+                    "UP" => Mathf.Ceil(value),
+                    "DOWN" => Mathf.Floor(value),
+                    _ => throw new ArgumentException($"'{mode}' is not a valid mode. Valid options: UP, DOWN"),
+                };
             }
         }
     }
