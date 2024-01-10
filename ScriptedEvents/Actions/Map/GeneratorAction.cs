3e89059f9826c8c088d4f0615e34bf0e7d35f45d
Olgierd <pieniek.olek@gmail.com>
2024-01-10 14:31:41 +0100

    add Genetators action

--- ScriptedEvents/Actions/Map/GeneratorAction.cs
+++ ScriptedEvents/Actions/Map/GeneratorAction.cs
@@ -1,10 +1,11 @@
 ﻿namespace ScriptedEvents.Actions
 {
     using System;
+    using System.Collections.Generic;
+    using Exiled.API.Features;
     using ScriptedEvents.API.Enums;
     using ScriptedEvents.API.Interfaces;
     using ScriptedEvents.Structures;
-    using Exiled.API.Features;
 
     public class GeneratorAction : IScriptAction, IHelpInfo
     {
@@ -21,27 +22,47 @@
         public ActionSubgroup Subgroup => ActionSubgroup.Map;
 
         /// <inheritdoc/>
-        public string Description => "Modifies tesla gates.";
+        public string Description => "Modifies genrators.";
 
         /// <inheritdoc/>
         public Argument[] ExpectedArguments => new[]
         {
-            new Argument("mode", typeof(string), "The mode to run. Valid options: PLAYERS, ROLETYPE, DISABLE, ENABLE", false),
+            new Argument("mode", typeof(string), "The mode to use. Valid options: OPEN, CLOSE, LOCK, UNLOCK, OVERCHARGE, ACTIVATE, DEACTIVATE", true),
         };
 
         /// <inheritdoc/>
         public ActionResponse Execute(Script script)
         {
+            if (Arguments.Length < 1) return new(MessageType.InvalidUsage, this, null, (object)ExpectedArguments);
+
             foreach (Generator generator in Generator.List)
             {
-                generator.IsEngaged = false;
-                generator.IsOpen = false;
-                generator.IsActivating = false;
-                generator.IsEngaged = false;
-                generator.IsUnlocked = false;
-                generator.DenyUnlockAndResetCooldown();
-                generator.Base.ServerSetFlag(MapGeneration.Distributors.Scp079Generator.GeneratorFlags.Open, false);
-                   
+                switch (Arguments[0].ToUpper())
+                {
+                    case "OPEN":
+                        generator.Base.ServerSetFlag(MapGeneration.Distributors.Scp079Generator.GeneratorFlags.Open, true);
+                        break;
+                    case "CLOSE":
+                        generator.Base.ServerSetFlag(MapGeneration.Distributors.Scp079Generator.GeneratorFlags.Open, false);
+                        break;
+                    case "LOCK":
+                        generator.IsUnlocked = false;
+                        break;
+                    case "UNLOCK":
+                        generator.IsUnlocked = true;
+                        break;
+                    case "OVERCHARGE":
+                        generator.IsEngaged = true;
+                        break;
+                    case "ACTIVATE":
+                        generator.IsActivating = true;
+                        break;
+                    case "DEACTIVATE":
+                        generator.IsActivating = false;
+                        break;
+                    default:
+                        return new(MessageType.InvalidOption, this, "mode", Arguments[0], "Valid options: OPEN, CLOSE, LOCK, UNLOCK, OVERCHARGE, ACTIVATE, DEACTIVATE");
+                }
             }
 
             return new(true);
