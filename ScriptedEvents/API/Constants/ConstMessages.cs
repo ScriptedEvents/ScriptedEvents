924eb66a2a82a41f12733afba29f869f796f4040
Thunder <thundermaker300@gmail.com>
2024-01-11 13:11:06 -0500

    Add long desc to GOTO/GOTOIF

--- ScriptedEvents/API/Constants/ConstMessages.cs
+++ ScriptedEvents/API/Constants/ConstMessages.cs
@@ -8,5 +8,10 @@
 - 'HeavyContainment' - Targets heavy containment rooms
 - 'Entrance' - Targets entrance rooms
 - The ID of a room. All valid room IDs can be found on Exiled's documentation at: https://exiled-team.github.io/EXILED/api/Exiled.API.Enums.RoomType.html";
+
+        public const string GotoInput = @"The following keywords can be used in place of a label:
+- START - Moves to the start of the script.
+- STOP - Immediately stops the script.
+- NEXT - Moves to the next line.";
     }
 }
