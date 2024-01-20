d0f6011f9dd8c1a3689d676facca487efd122b7f
Olgierd <pieniek.olek@gmail.com>
2024-01-20 22:22:22 +0100

    gave up

--- ScriptedEvents/Actions/Map/GeneratorAction.cs
+++ ScriptedEvents/Actions/Map/GeneratorAction.cs
@@ -1,21 +1,10 @@
 ﻿namespace ScriptedEvents.Actions
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using Exiled.API.Enums;
-    using Exiled.API.Extensions;
     using Exiled.API.Features;
-    using Mirror;
     using ScriptedEvents.API.Enums;
-    using ScriptedEvents.API.Features;
     using ScriptedEvents.API.Interfaces;
-    using ScriptedEvents.Commands.MainCommand;
     using ScriptedEvents.Structures;
-    using ScriptedEvents.Variables;
 
     public class GeneratorAction : IScriptAction, IHelpInfo
     {
@@ -38,7 +27,6 @@
         public Argument[] ExpectedArguments => new[]
         {
             new Argument("mode", typeof(string), "The mode to use. Valid options: OPEN, CLOSE, LOCK, UNLOCK, OVERCHARGE, ACTIVATE, DEACTIVATE", true),
-            new Argument("amount", typeof(int), "The amount of generators to affect.", false),
         };
 
         /// <inheritdoc/>
@@ -46,37 +34,7 @@
         {
             if (Arguments.Length < 1) return new(MessageType.InvalidUsage, this, null, (object)ExpectedArguments);
 
-            int max = 3;
-            if (Arguments.Length > 1)
-            {
-                if (!int.TryParse(Arguments[1], out max))
-                    return new(MessageType.NotANumber, this, "amount", Arguments[1]);
-            }
-
-            List<Generator> x = new List<Generator>(Generator.List);
-
-            // c# is dark fckn magic so chatgpt to the resque i guess\
-            // update: doesnt work
-            static List<T> ShuffleList<T>(List<T> list, int max)
-            {
-                Random random = new Random();
-                int n = Math.Min(list.Count, max);
-                List<T> shuffledList = new List<T>(list);
-
-                for (int i = n - 1; i > 0; i--)
-                {
-                    int randIndex = random.Next(0, i + 1);
-                    T temp = shuffledList[i];
-                    shuffledList[i] = shuffledList[randIndex];
-                    shuffledList[randIndex] = temp;
-                }
-
-                return shuffledList.GetRange(0, n);
-            }
-
-            x = ShuffleList(x, max);
-
-            foreach (Generator generator in x)
+            foreach (Generator generator in Generator.List)
             {
                 switch (Arguments[0].ToUpper())
                 {
