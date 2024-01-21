868b3f9814c09a87602ba668b919a7ed3dabcaff
Olgierd <pieniek.olek@gmail.com>
2024-01-21 01:16:19 +0100

    update GOTOIF

--- ScriptedEvents/Actions/Logic/GotoIfAction.cs
+++ ScriptedEvents/Actions/Logic/GotoIfAction.cs
@@ -8,6 +8,7 @@
     using ScriptedEvents.API.Features;
     using ScriptedEvents.API.Interfaces;
     using ScriptedEvents.Structures;
+    using ScriptedEvents.Variables.Interfaces;
 
     public class GotoIfAction : IScriptAction, ILogicAction, IHelpInfo, ILongDescription
     {
@@ -29,8 +30,7 @@
         /// <inheritdoc/>
         public Argument[] ExpectedArguments => new[]
         {
-            new Argument("trueLabel", typeof(string), "The label to jump to if the condition is TRUE, or a keyword (START/NEXT/STOP). Variables & Math are NOT supported.", true),
-            new Argument("falseLabel", typeof(string), "The label to jump to if the condition is FALSE, or a keyword (START/NEXT/STOP). Variables & Math are NOT supported.", true),
+            new Argument("label", typeof(string), "The label to jump to if the condition is TRUE. Variables & Math are NOT supported.", true),
             new Argument("condition", typeof(string), "The condition to check. Variables & Math are supported.", true),
         };
 
@@ -40,9 +40,45 @@
         /// <inheritdoc/>
         public ActionResponse Execute(Script script)
         {
-            if (Arguments.Length < 3) return new(MessageType.InvalidUsage, this, null, (object)ExpectedArguments);
+            // Support for old GOTOIF
+            if (script.Flags.Contains("OLD-GOTOIF"))
+            {
+                if (Arguments.Length < 3) return new(MessageType.InvalidUsage, this, null, (object)ExpectedArguments);
 
-            ConditionResponse outcome = ConditionHelperV2.Evaluate(string.Join(" ", Arguments.Skip(2)), script);
+                ConditionResponse deprecatedOutcome = ConditionHelperV2.Evaluate(string.Join(" ", Arguments.Skip(2)), script);
+                if (!deprecatedOutcome.Success)
+                    return new(false, $"IF execution error: {deprecatedOutcome.Message}", ActionFlags.FatalError);
+
+                if (deprecatedOutcome.Passed)
+                {
+                    script.DebugLog($"GOTOIF result: true. Jumping to line {Arguments[0]}.");
+                    if (!script.Jump(Arguments[0]))
+                    {
+                        if (Arguments[0].ToUpper() == "STOP")
+                            return new(true, flags: ActionFlags.StopEventExecution);
+
+                        return new(false, ErrorGen.Get(139, "trueLine", Arguments[0]));
+                    }
+                }
+                else
+                {
+                    script.DebugLog($"GOTOIF result: false. Jumping to line {Arguments[1]}.");
+                    if (!script.Jump(Arguments[1]))
+                    {
+                        if (Arguments[1].ToUpper() == "STOP")
+                            return new(true, flags: ActionFlags.StopEventExecution);
+
+                        return new(false, ErrorGen.Get(139, "falseLine", Arguments[1]));
+                    }
+                }
+
+                return new(true);
+            }
+
+            // Standard GOTOIF
+            if (Arguments.Length < 2) return new(MessageType.InvalidUsage, this, null, (object)ExpectedArguments);
+
+            ConditionResponse outcome = ConditionHelperV2.Evaluate(string.Join(" ", Arguments.Skip(1)), script);
             if (!outcome.Success)
                 return new(false, $"IF execution error: {outcome.Message}", ActionFlags.FatalError);
 
@@ -59,14 +95,7 @@
             }
             else
             {
-                script.DebugLog($"GOTOIF result: false. Jumping to line {Arguments[1]}.");
-                if (!script.Jump(Arguments[1]))
-                {
-                    if (Arguments[1].ToUpper() == "STOP")
-                        return new(true, flags: ActionFlags.StopEventExecution);
-
-                    return new(false, ErrorGen.Get(139, "falseLine", Arguments[1]));
-                }
+                script.DebugLog($"GOTOIF result: false. Proceeding to next line.");
             }
 
             return new(true);
