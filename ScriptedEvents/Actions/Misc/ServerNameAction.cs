5dc1b0baaf0cba0dba93fbf72284a9639ef25a5f
Olgierd <pieniek.olek@gmail.com>
2024-01-26 16:20:03 +0100

    server name action

---
+++ ScriptedEvents/Actions/Misc/ServerNameAction.cs
@@ -0,0 +1,41 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+    using Exiled.API.Features;
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+
+    public class ServerNameAction : IScriptAction, IHelpInfo
+    {
+        /// <inheritdoc/>
+        public string Name => "SERVERNAME";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.Misc;
+
+        /// <inheritdoc/>
+        public string Description => "Sets this server's name.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+            new Argument("name", typeof(string), "The name to apply.", true),
+        };
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            if (Arguments.Length < 1) return new(MessageType.InvalidUsage, this, null, (object)ExpectedArguments);
+
+            Server.Name = string.Join(" ", Arguments);
+            return new(true);
+        }
+    }
+}
