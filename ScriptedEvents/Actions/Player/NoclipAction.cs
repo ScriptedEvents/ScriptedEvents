9ea1d64ff5da7de1dee3f897ef2a32dc5f7b9558
Olgierd <pieniek.olek@gmail.com>
2024-02-13 20:09:44 +0100

    add NOCLIP action

---
+++ ScriptedEvents/Actions/Player/NoclipAction.cs
@@ -0,0 +1,52 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+
+    using Exiled.API.Features;
+
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+
+    public class NoclipAction : IScriptAction, IHelpInfo
+    {
+        /// <inheritdoc/>
+        public string Name => "NOCLIP";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.Player;
+
+        /// <inheritdoc/>
+        public string Description => "Grants or removes noclip to specified players.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+            new Argument("players", typeof(Player[]), "Players to change noclip state.", true),
+            new Argument("mode", typeof(bool), "Noclip mode.", true),
+        };
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            PlayerCollection players = (PlayerCollection)Arguments[0];
+            bool mode = (bool)Arguments[1];
+
+            foreach (Player player in players)
+            {
+                player.IsNoclipPermitted = mode;
+            }
+
+            return new(true);
+        }
+    }
+}
