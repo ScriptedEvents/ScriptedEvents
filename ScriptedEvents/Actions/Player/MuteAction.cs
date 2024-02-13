9a21b030fd55ab8931be567ad8ce9ca21bc8cb21
Olgierd <pieniek.olek@gmail.com>
2024-02-13 20:21:04 +0100

    add MUTE action

---
+++ ScriptedEvents/Actions/Player/MuteAction.cs
@@ -0,0 +1,50 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+
+    using Exiled.API.Features;
+
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+
+    public class MuteAction : IScriptAction, IHelpInfo
+    {
+        /// <inheritdoc/>
+        public string Name => "MUTE";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.Player;
+
+        /// <inheritdoc/>
+        public string Description => "Mutes specified players.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+            new Argument("players", typeof(Player[]), "Players to mute.", true),
+        };
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            PlayerCollection players = (PlayerCollection)Arguments[0];
+
+            foreach (Player player in players)
+            {
+                player.Mute();
+            }
+
+            return new(true);
+        }
+    }
+}
