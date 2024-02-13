c3caec4cff5a94807e8569ae824f11a043fc00af
Olgierd <pieniek.olek@gmail.com>
2024-02-13 20:04:01 +0100

    add KICK action

---
+++ ScriptedEvents/Actions/Player/KickAction.cs
@@ -0,0 +1,52 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+
+    using Exiled.API.Features;
+
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+
+    public class KickAction : IScriptAction, IHelpInfo
+    {
+        /// <inheritdoc/>
+        public string Name => "KICK";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.Player;
+
+        /// <inheritdoc/>
+        public string Description => "Kicks specified players.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+            new Argument("players", typeof(Player[]), "Players to kick.", true),
+            new Argument("reason", typeof(string), "Kick reason.", true),
+        };
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            PlayerCollection players = (PlayerCollection)Arguments[0];
+            string reason = (string)Arguments[1];
+
+            foreach (Player player in players)
+            {
+                player.Kick(reason);
+            }
+
+            return new(true);
+        }
+    }
+}
