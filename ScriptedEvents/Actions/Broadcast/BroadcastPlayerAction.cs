2146ca97aa9b286689f937dd85ca45913abe20c6
Olgierd <pieniek.olek@gmail.com>
2024-02-19 19:02:30 +0100

    Update BroadcastPlayerAction.cs

--- ScriptedEvents/Actions/Broadcast/BroadcastPlayerAction.cs
+++ ScriptedEvents/Actions/Broadcast/BroadcastPlayerAction.cs
@@ -1,14 +1,11 @@
 ﻿namespace ScriptedEvents.Actions
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
 
     using Exiled.API.Features;
 
     using ScriptedEvents.API.Enums;
     using ScriptedEvents.API.Extensions;
-    using ScriptedEvents.API.Features;
     using ScriptedEvents.API.Interfaces;
     using ScriptedEvents.Structures;
     using ScriptedEvents.Variables;
