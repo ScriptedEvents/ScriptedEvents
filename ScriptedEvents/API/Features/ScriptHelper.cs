fbcdf6cbd438b7a0a1781c4757968f4ab1a4b496
Olgierd <pieniek.olek@gmail.com>
2024-02-24 10:35:30 +0100

    fix trailing colon

--- ScriptedEvents/API/Features/ScriptHelper.cs
+++ ScriptedEvents/API/Features/ScriptHelper.cs
@@ -247,7 +247,7 @@ namespace ScriptedEvents.API.Features
                 // Obsolete check
                 if (newAction.IsObsolete(out string obsoleteReason) && !suppressWarnings && !script.SuppressWarnings)
                 {
-                    Log.Warn($"[L: {script.CurrentLine + 1}] [{scriptName}] Action {newAction.Name} is obsolete;: {obsoleteReason}");
+                    Log.Warn($"[L: {script.CurrentLine + 1}] [{scriptName}] Action {newAction.Name} is obsolete; {obsoleteReason}");
                 }
 
                 actionList.Add(newAction);
