151e53325e4d646b3a0c76cbac7970daded8d351
Olgierd <pieniek.olek@gmail.com>
2024-03-08 21:29:04 +0100

    update version i guess

--- ScriptedEvents/MainPlugin.cs
+++ ScriptedEvents/MainPlugin.cs
@@ -95,7 +95,7 @@
         public override string Author => "Thunder + Elektryk_Andrzej";
 
         /// <inheritdoc/>
-        public override Version Version => new(2, 8, 0);
+        public override Version Version => new(3, 0, 0);
 
         /// <inheritdoc/>
         public override Version RequiredExiledVersion => new(8, 7, 0);
