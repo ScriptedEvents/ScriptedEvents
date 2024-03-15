ff580b9e7e05b0bdd22fe679db136f6d90ac5e10
Olgierd <pieniek.olek@gmail.com>
2024-03-15 22:05:16 +0100

    upgrade MUTE

--- ScriptedEvents/Actions/Player/MuteAction.cs
+++ ScriptedEvents/Actions/Player/MuteAction.cs
@@ -5,6 +5,7 @@
     using Exiled.API.Features;
 
     using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Extensions;
     using ScriptedEvents.API.Interfaces;
     using ScriptedEvents.Structures;
 
@@ -31,17 +32,36 @@
         /// <inheritdoc/>
         public Argument[] ExpectedArguments => new[]
         {
-            new Argument("players", typeof(Player[]), "Players to mute.", true),
+            new Argument("mode", typeof(string), "The mode. Either SET or REMOVE", true),
+            new Argument("players", typeof(Player[]), "Players to change mute status for.", true),
+            new Argument("long-term", typeof(bool), "If TRUE, player will be muted even after rejoining.", true),
         };
 
         /// <inheritdoc/>
         public ActionResponse Execute(Script script)
         {
-            PlayerCollection players = (PlayerCollection)Arguments[0];
+            PlayerCollection players = (PlayerCollection)Arguments[1];
+            bool longTerm = (bool)Arguments[2];
+
+            Action<Player> playerAction;
+
+            switch (Arguments[0].ToUpper())
+            {
+                case "SET":
+                    playerAction = (plr) => { if (longTerm) plr.Mute(); else plr.IsMuted = true; };
+                    break;
+
+                case "REMOVE":
+                    playerAction = (plr) => { if (longTerm) plr.UnMute(); else plr.IsMuted = false; };
+                    break;
+
+                default:
+                    return new(false, $"Invalid mode '{Arguments[0].ToUpper()}' provided.");
+            }
 
             foreach (Player player in players)
             {
-                player.Mute();
+                playerAction(player);
             }
 
             return new(true);
