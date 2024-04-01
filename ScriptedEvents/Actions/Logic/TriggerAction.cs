e8a55a266335fdffc306239253f1ab7b039c84e4
Olgierd <pieniek.olek@gmail.com>
2024-04-01 16:31:55 +0200

    add TRIGGER action

---
+++ ScriptedEvents/Actions/Logic/TriggerAction.cs
@@ -0,0 +1,49 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Features;
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+
+    public class TriggerAction : IScriptAction, ILogicAction, IHelpInfo
+    {
+        /// <inheritdoc/>
+        public string Name => "TRIGGER";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.Logic;
+
+        /// <inheritdoc/>
+        public string Description => "Triggers a custom event, running scripts subscribed to that event.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+            new Argument("event", typeof(string), "Custom event name. This does not trigger Exiled events.", true),
+        };
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            string ev = (string)Arguments[0];
+
+            foreach (string scriptName in MainPlugin.CurrentCustomEventData[ev])
+            {
+                ScriptHelper.ReadAndRun(scriptName, script.Sender);
+            }
+
+            return new(true);
+        }
+    }
+}
