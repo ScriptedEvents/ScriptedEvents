638e822fb2774aba8f5a414bad56fa407d6af7dd
Olgierd <pieniek.olek@gmail.com>
2024-04-09 18:28:02 +0200

    BREAKING; combined intercom variables

--- ScriptedEvents/Variables/IntercomVariables.cs
+++ ScriptedEvents/Variables/IntercomVariables.cs
@@ -15,67 +15,9 @@
 
         public IVariable[] Variables { get; } = new IVariable[]
         {
-            new IntercomReady(),
-            new IntercomInUse(),
-            new IntercomCooldown(),
-            new IntercomUseTime(),
             new IntercomSpeaker(),
             new GeneralIntercom(),
         };
-    }
-
-    public class IntercomReady : IBoolVariable
-    {
-        /// <inheritdoc/>
-        public string Name => "{INTERCOMREADY}";
-
-        /// <inheritdoc/>
-        public string ReversedName => "{!INTERCOMREADY}";
-
-        /// <inheritdoc/>
-        public string Description => "Indicates whether or not the intercom is ready to be used.";
-
-        /// <inheritdoc/>
-        public bool Value => !Intercom.InUse && Intercom.RemainingCooldown <= 0;
-    }
-
-    public class IntercomInUse : IBoolVariable
-    {
-        /// <inheritdoc/>
-        public string Name => "{INTERCOMINUSE}";
-
-        /// <inheritdoc/>
-        public string ReversedName => "{!INTERCOMINUSE}";
-
-        /// <inheritdoc/>
-        public string Description => "Indicates whether or not the intercom is currently being used.";
-
-        /// <inheritdoc/>
-        public bool Value => Intercom.InUse;
-    }
-
-    public class IntercomCooldown : IFloatVariable
-    {
-        /// <inheritdoc/>
-        public string Name => "{INTERCOMCOOLDOWN}";
-
-        /// <inheritdoc/>
-        public string Description => "Indicates how much time is left on the intercom's cooldown.";
-
-        /// <inheritdoc/>
-        public float Value => (float)Intercom.RemainingCooldown;
-    }
-
-    public class IntercomUseTime : IFloatVariable
-    {
-        /// <inheritdoc/>
-        public string Name => "{INTERCOMUSETIME}";
-
-        /// <inheritdoc/>
-        public string Description => "Indicates how much time is left for the intercom speaker to speak.";
-
-        /// <inheritdoc/>
-        public float Value => Intercom.SpeechRemainingTime;
     }
 
     public class IntercomSpeaker : IFloatVariable, IPlayerVariable
