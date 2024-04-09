41612166f6310faf5d0bd3fdeeb834186f0f0960
Olgierd <pieniek.olek@gmail.com>
2024-04-09 18:28:56 +0200

    BREAKING; merged time variables

--- ScriptedEvents/Variables/WorldTimeVariables.cs
+++ ScriptedEvents/Variables/WorldTimeVariables.cs
@@ -15,99 +15,8 @@
         /// <inheritdoc/>
         public IVariable[] Variables { get; } = new IVariable[]
         {
-            new DayOfWeek(),
-            new DayOfMonth(),
-            new DayOfYear(),
-            new Month(),
-            new Year(),
-            new Tick(),
-            new Hour(),
             new Time(),
         };
-    }
-
-    public class DayOfWeek : IFloatVariable
-    {
-        /// <inheritdoc/>
-        public string Name => "{DAYOFWEEK}";
-
-        /// <inheritdoc/>
-        public string Description => "The current real-world day of the week, from 1-7, in UTC time.";
-
-        /// <inheritdoc/>
-        public float Value => ((int)DateTime.UtcNow.DayOfWeek) + 1;
-    }
-
-    public class DayOfMonth : IFloatVariable
-    {
-        /// <inheritdoc/>
-        public string Name => "{DAYOFMONTH}";
-
-        /// <inheritdoc/>
-        public string Description => "The current real-world day of the month, from 1-31, in UTC time.";
-
-        /// <inheritdoc/>
-        public float Value => DateTime.UtcNow.Day;
-    }
-
-    public class DayOfYear : IFloatVariable
-    {
-        /// <inheritdoc/>
-        public string Name => "{DAYOFYEAR}";
-
-        /// <inheritdoc/>
-        public string Description => "The current real-world day of the year, from 1-366, in UTC time.";
-
-        /// <inheritdoc/>
-        public float Value => DateTime.UtcNow.DayOfYear;
-    }
-
-    public class Month : IFloatVariable
-    {
-        /// <inheritdoc/>
-        public string Name => "{MONTH}";
-
-        /// <inheritdoc/>
-        public string Description => "The current real-world month, from 1-12, in UTC time.";
-
-        /// <inheritdoc/>
-        public float Value => DateTime.UtcNow.Month;
-    }
-
-    public class Year : IFloatVariable
-    {
-        /// <inheritdoc/>
-        public string Name => "{YEAR}";
-
-        /// <inheritdoc/>
-        public string Description => "The current real-world year, from 1-9999, in UTC time.";
-
-        /// <inheritdoc/>
-        public float Value => DateTime.UtcNow.Year;
-    }
-
-    public class Tick : ILongVariable
-    {
-        /// <inheritdoc/>
-        public string Name => "{TICK}";
-
-        /// <inheritdoc/>
-        public string Description => $"The amount of seconds since {new DateTime(1970, 1, 1):f}.";
-
-        /// <inheritdoc/>
-        public long Value => (long)(DateTime.UtcNow - MainPlugin.Epoch).TotalSeconds;
-    }
-
-    public class Hour : IFloatVariable
-    {
-        /// <inheritdoc/>
-        public string Name => "{HOUR}";
-
-        /// <inheritdoc/>
-        public string Description => $"The current real-world hour, from 0-23.";
-
-        /// <inheritdoc/>
-        public float Value => DateTime.UtcNow.Hour;
     }
 
     public class Time : IFloatVariable, IArgumentVariable, ILongDescription
@@ -135,7 +44,7 @@ SECOND: 0-59.
 MINUTE: 0-59. 
 HOUR: 0-23.
 YEAR: The amount of years since the birth of Christ.
-DAYOFWEEK: 1-7.
+DAYOFWEEK: 1-7 (This follows the US system, where Saturday is the first day of the week).
 DAYOFMONTH: 1-31.
 DAYOFYEAR: 1-366.";
 
