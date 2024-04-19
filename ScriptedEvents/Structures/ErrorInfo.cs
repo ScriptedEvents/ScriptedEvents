d5fdd1f43d1864b17b3c6d27733225cf11171e5f
Thunder <thundermaker300@gmail.com>
2024-04-19 19:12:00 -0400

    Create ErrorCode enum

--- ScriptedEvents/Structures/ErrorInfo.cs
+++ ScriptedEvents/Structures/ErrorInfo.cs
@@ -1,5 +1,7 @@
 ﻿namespace ScriptedEvents.Structures
 {
+    using ScriptedEvents.API.Enums;
+
     /// <summary>
     /// Holds information about an SE error.
     /// </summary>
@@ -8,20 +10,27 @@
         /// <summary>
         /// Initializes a new instance of the <see cref="ErrorInfo"/> struct.
         /// </summary>
-        /// <param name="id">The error code.</param>
+        /// <param name="id">The error ID.</param>
+        /// <param name="code">The error code.</param>
         /// <param name="info">Short description.</param>
         /// <param name="longInfo">Long description.</param>
-        public ErrorInfo(int id, string info, string longInfo)
+        public ErrorInfo(int id, ErrorCode code, string info, string longInfo)
         {
             Id = id;
+            Code = code;
             Info = info;
             LongDescription = longInfo;
         }
 
         /// <summary>
-        /// Gets the error code.
+        /// Gets the error ID.
         /// </summary>
         public int Id { get; }
+
+        /// <summary>
+        /// Gets the error code.
+        /// </summary>
+        public ErrorCode Code { get; }
 
         /// <summary>
         /// Gets the short description of the error.
