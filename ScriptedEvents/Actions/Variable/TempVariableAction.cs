f8c9b588c7672705cd3bc7a9fd2436e604b1bf18
Olgierd <pieniek.olek@gmail.com>
2024-04-28 14:38:40 +0200

    add TEMP action

---
+++ ScriptedEvents/Actions/Variable/TempVariableAction.cs
@@ -0,0 +1,57 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Extensions;
+    using ScriptedEvents.API.Features;
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+    using ScriptedEvents.Variables;
+
+    public class TempVariableAction : IScriptAction, IHelpInfo
+    {
+        /// <inheritdoc/>
+        public string Name => "TEMP";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.Variable;
+
+        /// <inheritdoc/>
+        public string Description => "Saves a new  variable '{^}' in the local scope.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+            new Argument("value", typeof(object), "The value to store. Math is supported.", true),
+        };
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            string input = Arguments.JoinMessage(0);
+            input = VariableSystem.ReplaceVariables(input, script);
+
+            try
+            {
+                float value = (float)ConditionHelperV2.Math(input);
+                script.AddVariable("{^}", "User-defined variable.", value.ToString());
+                return new(true);
+            }
+            catch
+            {
+                script.AddVariable("{^}", "User-defined variable.", input);
+                return new(true);
+            }
+        }
+    }
+}
