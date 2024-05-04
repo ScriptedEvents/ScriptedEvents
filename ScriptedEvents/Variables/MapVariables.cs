8d76a0814589cf3644daadf1d6d6d33927109b56
Olgierd <pieniek.olek@gmail.com>
2024-05-04 15:20:19 +0200

    Update MapVariables.cs

--- ScriptedEvents/Variables/MapVariables.cs
+++ ScriptedEvents/Variables/MapVariables.cs
@@ -2,6 +2,7 @@
 {
 #pragma warning disable SA1402 // File may only contain a single type.
     using System;
+    using System.Collections.Generic;
     using System.Linq;
 
     using Exiled.API.Enums;
@@ -27,7 +28,55 @@
             new DoorState(),
             new Overcharged(),
             new Generators(),
+            new RandomRoom(),
+            new RandomDoor(),
         };
+    }
+
+    public class RandomDoor : IStringVariable, INeedSourceVariable, IArgumentVariable
+    {
+        /// <inheritdoc/>
+        public string Name => "{RANDOMDOOR}";
+
+        /// <inheritdoc/>
+        public string Description => "Gets the DoorType of a random door. Can be filtered by zone.";
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments { get; } = new[]
+        {
+            new Argument("zone", typeof(ZoneType), "A zone to filter by (optional).", false),
+        };
+
+        /// <inheritdoc/>
+        public Script Source { get; set; }
+
+        /// <inheritdoc/>
+        public string Value
+        {
+            get
+            {
+                ZoneType filter = ZoneType.Unspecified;
+
+                if (Arguments.Length > 0)
+                    filter = (ZoneType)Arguments[0];
+
+                IEnumerable<Door> validDoors = Door.List;
+
+                if (filter is not ZoneType.Unspecified)
+                {
+                    validDoors = validDoors.Where(door => door.Zone.HasFlag(filter));
+                }
+
+                List<Door> newList = validDoors.ToList();
+                return newList[UnityEngine.Random.Range(0, newList.Count)].Type.ToString();
+            }
+        }
     }
 
     public class Decontaminated : IBoolVariable
@@ -117,6 +166,50 @@
                     default:
                         throw new Exception($"Mode {Arguments[0]} is not ENGAGED/ACTIVATING/UNLOCKED/OPENED or CLOSED.");
                 }
+            }
+        }
+    }
+
+    public class RandomRoom : IStringVariable, IArgumentVariable, INeedSourceVariable
+    {
+        /// <inheritdoc/>
+        public string Name => "{RANDOMROOM}";
+
+        /// <inheritdoc/>
+        public string Description => "Gets the RoomType of a random room. Can be filtered by zone.";
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments { get; } = new[]
+        {
+            new Argument("zone", typeof(ZoneType), "A zone to filter by (optional).", false),
+        };
+
+        /// <inheritdoc/>
+        public Script Source { get; set; }
+
+        /// <inheritdoc/>
+        public string Value
+        {
+            get
+            {
+                ZoneType filter = ZoneType.Unspecified;
+
+                if (Arguments.Length > 0)
+                    filter = (ZoneType)Arguments[0];
+
+                IEnumerable<Room> validRooms = Room.List.Where(room => room.Type != RoomType.Pocket);
+
+                if (filter is not ZoneType.Unspecified)
+                    validRooms = validRooms.Where(room => room.Zone.HasFlag(filter));
+
+                List<Room> newList = validRooms.ToList();
+                return newList[UnityEngine.Random.Range(0, newList.Count)].Type.ToString();
             }
         }
     }
