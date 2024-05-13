a0d1bda9bf23fc932e356895eac2abcffc73a3de
Olgierd <pieniek.olek@gmail.com>
2024-05-13 12:42:52 +0200

    merge RESETLIGHTCOLOR with LIGHTCOLOR

--- ScriptedEvents/Actions/Lights/LightColorAction.cs
+++ ScriptedEvents/Actions/Lights/LightColorAction.cs
@@ -2,15 +2,14 @@
 {
     using System;
 
-    using Exiled.API.Enums;
     using Exiled.API.Features;
 
     using ScriptedEvents.API.Constants;
     using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Extensions;
     using ScriptedEvents.API.Features;
     using ScriptedEvents.API.Interfaces;
     using ScriptedEvents.Structures;
-    using ScriptedEvents.Variables;
     using UnityEngine;
 
     public class LightColorAction : IScriptAction, IHelpInfo, ILongDescription
@@ -39,34 +38,54 @@
         /// <inheritdoc/>
         public Argument[] ExpectedArguments => new[]
         {
+            new OptionsArgument("mode", true,
+                new("SET", "Sets the light color. Red, Green and Blue values need to be provided when using."),
+                new("RESET", "Resets the light color.")),
             new Argument("room", typeof(Room[]), "The room to change the color of.", true),
-            new Argument("red", typeof(float), "The red component of the color", true),
-            new Argument("green", typeof(float), "The green component of the color", true),
-            new Argument("blue", typeof(float), "The blue component of the color", true),
+            new Argument("red", typeof(float), "The red component of the color.", false),
+            new Argument("green", typeof(float), "The green component of the color.", false),
+            new Argument("blue", typeof(float), "The blue component of the color.", false),
         };
 
         /// <inheritdoc/>
         public ActionResponse Execute(Script script)
         {
-            Room[] rooms = (Room[])Arguments[0];
-            float r = (float)Arguments[1];
-            float g = (float)Arguments[2];
-            float b = (float)Arguments[3];
+            Room[] rooms = (Room[])Arguments[1];
 
-            if (r < 0)
-                return new(false, "The red component of the color must be greater than 0.");
-
-            if (g < 0)
-                return new(false, "The green component of the color must be greater than 0.");
-
-            if (b < 0)
-                return new(false, "The blue component of the color must be greater than 0.");
-
-            Color c = new(r / 255f, g / 255f, b / 255f);
-
-            foreach (Room room in rooms)
+            switch (Arguments[0].ToUpper())
             {
-                room.Color = c;
+                case "SET":
+                    if (Arguments.Length < 5) return new(false, ErrorGen.Generate(ErrorCode.MissingArguments));
+
+                    float r = (float)Arguments[2];
+                    float g = (float)Arguments[3];
+                    float b = (float)Arguments[4];
+
+                    if (r < 0)
+                        return new(false, "The red component of the color must be greater than 0.");
+
+                    if (g < 0)
+                        return new(false, "The green component of the color must be greater than 0.");
+
+                    if (b < 0)
+                        return new(false, "The blue component of the color must be greater than 0.");
+
+                    Color c = new(r / 255f, g / 255f, b / 255f);
+
+                    foreach (Room room in rooms)
+                    {
+                        room.Color = c;
+                    }
+
+                    break;
+                case "RESET":
+                    foreach (Room room in rooms)
+                    {
+                        if (room.RoomLightController is not null)
+                            room.ResetColor();
+                    }
+
+                    break;
             }
 
             return new(true);
