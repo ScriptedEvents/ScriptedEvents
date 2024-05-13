3bf4dc70d29f6977ec98ed2d907a8a94a5d1782f
Olgierd <pieniek.olek@gmail.com>
2024-05-13 12:45:21 +0200

    add CALL functionality for GOTO

--- ScriptedEvents/Actions/Logic/GotoAction.cs
+++ ScriptedEvents/Actions/Logic/GotoAction.cs
@@ -2,12 +2,11 @@
 {
     using System;
 
-    using ScriptedEvents.API.Constants;
     using ScriptedEvents.API.Enums;
     using ScriptedEvents.API.Interfaces;
     using ScriptedEvents.Structures;
 
-    public class GotoAction : IScriptAction, ILogicAction, IHelpInfo, ILongDescription
+    public class GotoAction : IScriptAction, ILogicAction, IHelpInfo
     {
         /// <inheritdoc/>
         public string Name => "GOTO";
@@ -25,25 +24,29 @@
         public ActionSubgroup Subgroup => ActionSubgroup.Logic;
 
         /// <inheritdoc/>
-        public string Description => "Moves to the provided label.";
+        public string Description => "Moves to the provided label. Use 'START' to go to the start of the script.";
 
         /// <inheritdoc/>
         public Argument[] ExpectedArguments => new[]
         {
             new Argument("label", typeof(string), "The label to move to.", true),
+            new Argument("goBackSave", typeof(bool), "If TRUE, the line in which this action is situated in will be saved to the GOBACK stack. To learn more, read GOBACK action.", false),
         };
-
-        /// <inheritdoc/>
-        public string LongDescription => ConstMessages.GotoInput;
 
         /// <inheritdoc/>
         public ActionResponse Execute(Script script)
         {
+            int curLine = script.CurrentLine;
             string label = (string)Arguments[0];
 
             if (!script.Jump(label))
             {
                 return new(false, "Invalid label provided.");
+            }
+
+            if (Arguments.Length >= 2 && (bool)Arguments[1])
+            {
+                script.CallLines.Add(curLine);
             }
 
             return new(true);
