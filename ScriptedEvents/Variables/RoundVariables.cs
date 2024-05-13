5361d846e67fb8ee38b1341eeacb0c346f0c9e39
Olgierd <pieniek.olek@gmail.com>
2024-05-13 17:45:12 +0200

    move most vars from round to time cat

--- ScriptedEvents/Variables/RoundVariables.cs
+++ ScriptedEvents/Variables/RoundVariables.cs
@@ -1,7 +1,11 @@
 ﻿namespace ScriptedEvents.Variables.Round
 {
+    using System;
 #pragma warning disable SA1402 // File may only contain a single type
+
     using Exiled.API.Features;
+
+    using ScriptedEvents.Structures;
     using ScriptedEvents.Variables.Interfaces;
 
     public class RoundVariables : IVariableGroup
@@ -13,15 +17,8 @@
         public IVariable[] Variables { get; } = new IVariable[]
         {
             new UptimeRound(),
-            new RoundEnded(),
-            new RoundInProgress(),
-            new RoundStarted(),
-
-            new RoundMinutes(),
-            new RoundSeconds(),
-            new LobbyTime(),
-            new RoundLock(),
             new LobbyLock(),
+            new GeneralRound(),
         };
     }
 
@@ -35,116 +32,6 @@
 
         /// <inheritdoc/>
         public float Value => Round.UptimeRounds;
-    }
-
-    public class RoundEnded : IBoolVariable
-    {
-        /// <inheritdoc/>
-        public string Name => "{ROUNDENDED}";
-
-        /// <inheritdoc/>
-        public string ReversedName => "{!ROUNDENDED}";
-
-        /// <inheritdoc/>
-        public string Description => "Whether or not the round has ended.";
-
-        /// <inheritdoc/>
-        public bool Value => Round.IsEnded;
-    }
-
-    public class RoundInProgress : IBoolVariable
-    {
-        /// <inheritdoc/>
-        public string Name => "{ROUNDINPROGRESS}";
-
-        /// <inheritdoc/>
-        public string ReversedName => "{!ROUNDINPROGRESS}";
-
-        /// <inheritdoc/>
-        public string Description => "Whether or not the round is in progress.";
-
-        /// <inheritdoc/>
-        public bool Value => Round.InProgress;
-    }
-
-    public class RoundStarted : IBoolVariable
-    {
-        /// <inheritdoc/>
-        public string Name => "{ROUNDSTARTED}";
-
-        /// <inheritdoc/>
-        public string ReversedName => "{!ROUNDSTARTED}";
-
-        /// <inheritdoc/>
-        public string Description => "Whether or not the round has started.";
-
-        /// <inheritdoc/>
-        public bool Value => Round.IsStarted;
-    }
-
-    public class RoundMinutes : IFloatVariable
-    {
-        /// <inheritdoc/>
-        public string Name => "{ROUNDMINUTES}";
-
-        /// <inheritdoc/>
-        public string Description => "The amount of elapsed round time, in minutes.";
-
-        /// <inheritdoc/>
-        public float Value => (float)Round.ElapsedTime.TotalMinutes;
-    }
-
-    public class RoundSeconds : IFloatVariable
-    {
-        /// <inheritdoc/>
-        public string Name => "{ROUNDSECONDS}";
-
-        /// <inheritdoc/>
-        public string Description => "The amount of elapsed round time, in seconds.";
-
-        /// <inheritdoc/>
-        public float Value => (float)Round.ElapsedTime.TotalSeconds;
-    }
-
-    public class LobbyTime : IFloatVariable
-    {
-        /// <inheritdoc/>
-        public string Name => "{LOBBYTIME}";
-
-        /// <inheritdoc/>
-        public string Description => "The amount of time remaining before the round starts. -1 if round already started.";
-
-        /// <inheritdoc/>
-        public float Value
-        {
-            get
-            {
-                if (Round.IsStarted)
-                    return -1f;
-
-                return Round.LobbyWaitingTime;
-            }
-        }
-    }
-
-    public class RoundLock : IBoolVariable
-    {
-        /// <inheritdoc/>
-        public string Name => "{ROUNDLOCKED}";
-
-        public string ReversedName => "{!ROUNDLOCKED}";
-
-        /// <inheritdoc/>
-        public string Description => "Returns the roundlock setting status.";
-
-        /// <inheritdoc/>
-        public bool Value
-        {
-            get
-            {
-                return Round.IsLocked;
-            }
-        }
     }
 
     public class LobbyLock : IBoolVariable
@@ -165,5 +52,47 @@
                 return Round.IsLobbyLocked;
             }
         }
+    }
+
+    public class GeneralRound : IBoolVariable, IArgumentVariable
+    {
+        /// <inheritdoc/>
+        public string Name => "{ROUND}";
+
+        public string ReversedName => "{!ROUND}";
+
+        /// <inheritdoc/>
+        public string Description => "All-in-one variable for round related information.";
+
+        public Argument[] ExpectedArguments => new[]
+        {
+            new OptionsArgument("mode", true,
+                new("LOCKED", "Retruns the roundlock status."),
+                new("STARTED", "TRUE if round has started."),
+                new("INPROGRESS", "TRUE if round is in progress, neither started or ended."),
+                new("ENDED", "TRUE if round has ended.")),
+        };
+
+        /// <inheritdoc/>
+        public bool Value
+        {
+            get
+            {
+                string mode = (string)Arguments[0];
+
+                return mode.ToUpper() switch
+                {
+                    "LOCKED" => Round.IsLocked,
+                    "STARTED" => Round.IsStarted,
+                    "INPROGRESS" => Round.InProgress,
+                    "ENDED" => Round.IsEnded,
+                    _ => throw new ArgumentException("No mode provided")
+                };
+            }
+        }
+
+        public string[] RawArguments { get; set; }
+
+        public object[] Arguments { get; set; }
     }
 }
