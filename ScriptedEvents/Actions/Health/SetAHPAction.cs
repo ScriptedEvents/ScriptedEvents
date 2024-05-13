1ec11bdbb577095b6073e3e02a68c3941a80e962
Olgierd <pieniek.olek@gmail.com>
2024-05-13 12:40:36 +0200

    merge ADVAHP into AHP

--- ScriptedEvents/Actions/Health/SetAHPAction.cs
+++ ScriptedEvents/Actions/Health/SetAHPAction.cs
@@ -5,10 +5,8 @@
     using Exiled.API.Features;
 
     using ScriptedEvents.API.Enums;
-    using ScriptedEvents.API.Features;
     using ScriptedEvents.API.Interfaces;
     using ScriptedEvents.Structures;
-    using ScriptedEvents.Variables;
 
     public class SetAHPAction : IScriptAction, IHelpInfo
     {
@@ -28,13 +26,18 @@
         public object[] Arguments { get; set; }
 
         /// <inheritdoc/>
-        public string Description => "Add AHP to the targeted players.";
+        public string Description => "Add AHP to the targeted players, with advanced settings.";
 
         /// <inheritdoc/>
         public Argument[] ExpectedArguments => new[]
         {
             new Argument("players", typeof(Player[]), "The players to affect.", true),
             new Argument("health", typeof(float), "The amount of artificial health to add to the player.", true),
+            new Argument("limit", typeof(float), "The upper limit of AHP. Defaults to 75.", false),
+            new Argument("decay", typeof(float), "The AHP decay rate (how much AHP is lost per second). Defaults to 1.2.", false),
+            new Argument("efficacy", typeof(float), "The percent of incoming damage absorbed by AHP. Defaults to 0.7 (70%).", false),
+            new Argument("sustain", typeof(float), "The amount of time (in seconds) before the AHP begins to decay. Defaults to 0.", false),
+            new Argument("persistent", typeof(bool), "Whether or not the AHP process is removed entirely when the AHP reaches 0. Defaults to FALSE.", false),
         };
 
         /// <inheritdoc/>
@@ -43,11 +46,42 @@
             PlayerCollection plys = (PlayerCollection)Arguments[0];
             float hp = (float)Arguments[1];
 
-            if (hp < 0)
-                return new(MessageType.LessThanZeroNumber, this, "health", hp);
+            float limit = 75;
+            if (Arguments.Length > 2)
+            {
+                limit = (float)Arguments[2];
+                if (limit < 0)
+                    return new(MessageType.LessThanZeroNumber, this, "limit", limit);
+            }
+
+            float decay = 1.2f;
+            if (Arguments.Length > 3)
+            {
+                decay = (float)Arguments[3];
+                if (decay < 0)
+                    return new(MessageType.LessThanZeroNumber, this, "decay", limit);
+            }
+
+            float efficacy = 0.7f;
+            if (Arguments.Length > 4)
+            {
+                efficacy = (float)Arguments[4];
+                if (efficacy < 0)
+                    return new(MessageType.LessThanZeroNumber, this, "efficacy", limit);
+            }
+
+            float sustain = 0f;
+            if (Arguments.Length > 5)
+            {
+                sustain = (float)Arguments[5];
+                if (sustain < 0)
+                    return new(MessageType.LessThanZeroNumber, this, "sustain", limit);
+            }
+
+            bool persistent = Arguments.Length > 6 && (bool)Arguments[6];
 
             foreach (Player ply in plys)
-                ply.AddAhp(hp);
+                ply.AddAhp(hp, limit, decay, efficacy, sustain, persistent);
 
             return new(true);
         }
