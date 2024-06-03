7157463a17dd67c1abf3378f00ec43afa454a790
Olgierd <pieniek.olek@gmail.com>
2024-06-03 21:42:38 +0200

    add CLEARBROADCAST action

---
+++ ScriptedEvents/Actions/Broadcast/ClearBroadcastAction.cs
@@ -0,0 +1,49 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+
+    using Exiled.API.Features;
+
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+
+    public class ClearBroadcastAction : IScriptAction, IHelpInfo
+    {
+        /// <inheritdoc/>
+        public string Name => "CLEARBROADCAST";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.Broadcast;
+
+        /// <inheritdoc/>
+        public string Description => "Clears specific players' broadcast queue.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+            new Argument("players", typeof(Player[]), "The players to affect.", true),
+        };
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            PlayerCollection players = (PlayerCollection)Arguments[0];
+            foreach (Player player in players)
+            {
+                player.ClearBroadcasts();
+            }
+
+            return new(true);
+        }
+    }
+}
