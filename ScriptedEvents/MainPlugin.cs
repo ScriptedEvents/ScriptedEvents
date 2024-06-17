c1190e3862447d3e809a715f98595898c331cdd6
Olgierd <pieniek.olek@gmail.com>
2024-06-17 14:29:45 +0200

    Update MainPlugin.cs

--- ScriptedEvents/MainPlugin.cs
+++ ScriptedEvents/MainPlugin.cs
@@ -73,7 +73,7 @@
         public override string Name => "ScriptedEvents";
 
         /// <inheritdoc/>
-        public override string Author => "Thunder + Elektryk_Andrzej";
+        public override string Author => "Thunder and Elektryk_Andrzej";
 
         /// <inheritdoc/>
         public override Version Version => new(3, 0, 0);
