fb085c8699effb8d08b944d0865b7b55d82f2e75
Olgierd <pieniek.olek@gmail.com>
2024-06-17 14:27:15 +0200

    remove GET, expand DEL to DELETE for playerdata

--- ScriptedEvents/Actions/Variable/PlayerDataAction.cs
+++ ScriptedEvents/Actions/Variable/PlayerDataAction.cs
@@ -32,12 +32,11 @@
         public Argument[] ExpectedArguments => new[]
         {
             new OptionsArgument("mode", true,
-                new("GET", "Creates a variable containing the value of the player data."),
                 new("SET", "Sets the player data."),
-                new("DEL", "Deletes the player data.")),
+                new("DELETE", "Deletes the player data.")),
             new Argument("players", typeof(PlayerCollection), "The players to affect.", true),
             new Argument("keyName", typeof(string), "The name of the key.", true),
-            new Argument("value", typeof(string), "GET - The variable to create containing the value of the accessed key, SET - The new value of the key, DELETE - N/A", false),
+            new Argument("value", typeof(string), "The new value of the key. Not used when using mode 'DELETE'.", false),
         };
 
         /// <inheritdoc/>
@@ -47,26 +46,11 @@
 
             switch (((string)Arguments[0]).ToUpper())
             {
-                case "GET" when Arguments.Length < 4:
                 case "SET" when Arguments.Length < 4:
                     return new(MessageType.InvalidUsage, this, null, (object)ExpectedArguments);
 
-                case "GET":
-                    if (players.Length > 1)
-                        return new(false, "The 'GET' mode of the PLAYERDATA action only works on variables with exactly one player!");
-
-                    string varName = RawArguments[3];
-                    string keyName = (string)Arguments[2];
-                    Player ply1 = players[0];
-
-                    if (ply1.SessionVariables.ContainsKey(keyName))
-                        VariableSystemV2.DefineVariable(varName, $"The result of a PLAYERDATA execution using 'GET' on player '{ply1.DisplayNickname}' with key '{varName}'.", ply1.SessionVariables[keyName].ToString(), script);
-                    else
-                        VariableSystemV2.DefineVariable(varName, $"The result of a PLAYERDATA execution using 'GET' on player '{ply1.DisplayNickname}' with key '{varName}'.", "NONE", script);
-                    break;
-
                 case "SET":
-                    keyName = (string)Arguments[2];
+                    string keyName = (string)Arguments[2];
                     foreach (Player ply in players)
                     {
                         if (ply.SessionVariables.ContainsKey(keyName))
@@ -76,7 +60,7 @@
                     }
 
                     break;
-                case "DEL":
+                case "DELETE":
                     keyName = (string)Arguments[2];
                     foreach (Player ply in players)
                     {
