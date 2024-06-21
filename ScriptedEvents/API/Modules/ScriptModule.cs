48b017fd74ac8c469e474f82b259999c21ca342a
Olgierd <pieniek.olek@gmail.com>
2024-06-21 19:50:51 +0200

    i inadvertedly fucked something up

--- ScriptedEvents/API/Modules/ScriptModule.cs
+++ ScriptedEvents/API/Modules/ScriptModule.cs
@@ -236,71 +236,62 @@ namespace ScriptedEvents.API.Modules
             string[] array = allText.Split('\n');
             for (int currentline = 0; currentline < array.Length; currentline++)
             {
-                if (currentline >= array.Length)
-                    throw new ArgumentOutOfRangeException(nameof(currentline), "Current line index is out of range.");
-
                 array[currentline] = array[currentline].TrimStart();
-                List<string> actionParts = ListPool<string>.Pool.Get();
 
-                foreach (string str in array[currentline].Split(' '))
-                {
-                    if (string.IsNullOrWhiteSpace(str))
-                        continue;
-                    actionParts.Add(str);
-                }
-
-                if (actionParts[0] == null)
-                    throw new ArgumentOutOfRangeException("Action parts array is empty or null.");
-
-                string keyword = actionParts[0].ToUpper();
-                Logger.Warn(string.Join(" ", actionParts));
-
-                if (string.IsNullOrWhiteSpace(keyword))
+                // NoAction
+                string action = array[currentline];
+                if (string.IsNullOrWhiteSpace(action))
                 {
                     actionList.Add(new NullAction("BLANK LINE"));
                     continue;
                 }
-                else if (keyword.StartsWith("##"))
+                else if (action.StartsWith("##"))
                 {
                     inMultilineComment = !inMultilineComment;
                     actionList.Add(new NullAction("COMMENT"));
                     continue;
                 }
-                else if (keyword.StartsWith("#") || inMultilineComment)
+                else if (action.StartsWith("#") || inMultilineComment)
                 {
                     actionList.Add(new NullAction("COMMENT"));
                     continue;
                 }
-                else if (keyword == "!--")
+                else if (action.StartsWith("!--"))
                 {
-                    if (actionParts.Count < 2)
-                    {
-                        Logger.Warn("Flag definition syntax was used, but no flag name was provided.", script);
-                        continue;
-                    }
+                    string flag = action.Replace("!--", string.Empty).Trim();
 
-                    string flagName = actionParts[1].ToUpper();
-                    if (!script.HasFlag(flagName))
+                    if (!script.HasFlag(flag))
                     {
-                        string[] arguments = actionParts.Skip(2).ToArray();
-                        Flag flag = new(flagName, arguments);
-                        script.Flags.Add(flag);
+                        string[] arguments = flag.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        Flag fl = new(arguments[0], arguments.Skip(1));
+                        script.Flags.Add(fl);
                     }
                     else if (!suppressWarnings)
                     {
-                        Logger.Warn(ErrorGen.Get(ErrorCode.MultipleFlagDefs, flagName, scriptName));
+                        Logger.Warn(ErrorGen.Get(ErrorCode.MultipleFlagDefs, flag, scriptName));
                     }
 
                     actionList.Add(new NullAction("FLAG DEFINE"));
                     continue;
                 }
 
+                List<string> actionParts = ListPool<string>.Pool.Get();
+
+                foreach (string str in action.Split(' '))
+                {
+                    if (string.IsNullOrWhiteSpace(str))
+                        continue;
+
+                    actionParts.Add(str);
+                }
+
+                string keyword = actionParts[0].RemoveWhitespace();
+
+                // Std labels
                 if (keyword.EndsWith(":"))
                 {
-                    if (keyword.Length == 0)
-                        throw new ArgumentOutOfRangeException("Keyword is empty.");
+                    string labelName = action.Remove(keyword.Length - 1, 1).RemoveWhitespace();
 
-                    string labelName = keyword.Remove(keyword.Length - 1, 1).RemoveWhitespace();
                     if (!script.Labels.ContainsKey(labelName))
                         script.Labels.Add(labelName, currentline);
                     else if (!suppressWarnings)
@@ -309,6 +300,7 @@ namespace ScriptedEvents.API.Modules
                     continue;
                 }
 
+                // Function labels
                 if (keyword == "->")
                 {
                     if (actionParts.Count < 2)
@@ -316,29 +308,25 @@ namespace ScriptedEvents.API.Modules
                         Logger.Error($"A function label syntax has been used, but no name has been provided.", script);
                         continue;
                     }
-
                     string labelName = actionParts[1].RemoveWhitespace();
-
                     if (!script.FunctionLabels.ContainsKey(labelName))
                         script.FunctionLabels.Add(labelName, currentline);
                     else if (!suppressWarnings)
                         Logger.Warn(ErrorGen.Get(ErrorCode.MultipleLabelDefs, labelName, scriptName));
-
                     actionList.Add(new StartFunctionAction());
                     continue;
                 }
 
-                Logger.Debug($"Queuing action {keyword} {string.Join(", ", actionParts.Skip(1))}", script);
-
+                Logger.Debug($"Queuing action {keyword} {string.Join(", ", actionParts.Skip(1))}", script);
                 if (!TryGetActionType(keyword, out Type actionType))
                 {
-                    if (CustomActions.TryGetValue(keyword.ToUpper(), out CustomAction customAction))
+                    // Check for custom actions
+                    if (CustomActions.TryGetValue(keyword, out CustomAction customAction))
                     {
                         CustomAction customAction1 = new(customAction.Name, customAction.Action)
                         {
                             Arguments = actionParts.Skip(1).Select(str => str.RemoveWhitespace()).ToArray(),
                         };
-
                         actionList.Add(customAction1);
                         ListPool<string>.Pool.Return(actionParts);
                         continue;
@@ -346,7 +334,6 @@ namespace ScriptedEvents.API.Modules
 
                     if (!suppressWarnings)
                         Logger.Warn(ErrorGen.Get(ErrorCode.InvalidAction, keyword.RemoveWhitespace(), scriptName), script);
-
                     actionList.Add(new NullAction("ERROR"));
                     continue;
                 }
@@ -354,6 +341,7 @@ namespace ScriptedEvents.API.Modules
                 IAction newAction = Activator.CreateInstance(actionType) as IAction;
                 newAction.RawArguments = actionParts.Skip(1).Select(str => str.RemoveWhitespace()).ToArray();
 
+                // Obsolete check
                 if (newAction.IsObsolete(out string obsoleteReason) && !suppressWarnings && !script.SuppressWarnings)
                 {
                     Logger.Warn($"Action {newAction.Name} is obsolete; {obsoleteReason}", script);
