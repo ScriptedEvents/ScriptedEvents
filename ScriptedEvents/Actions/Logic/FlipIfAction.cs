60a40263462638adcbe982c818885e68275e100c
Olgierd <pieniek.olek@gmail.com>
2024-06-22 14:00:29 +0200

    add FLIPIF action

---
+++ ScriptedEvents/Actions/Logic/FlipIfAction.cs
@@ -0,0 +1,39 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+
+    public class FlipIfAction : IScriptAction, ILogicAction, IHelpInfo, IIgnoresIfActionBlock
+    {
+        /// <inheritdoc/>
+        public string Name => "FLIPIF";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.Logic;
+
+        /// <inheritdoc/>
+        public string Description => "Flips the current IF action action block.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => Array.Empty<Argument>();
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            script.IfActionBlocksExecution = !script.IfActionBlocksExecution;
+            return new(true);
+        }
+    }
+}
