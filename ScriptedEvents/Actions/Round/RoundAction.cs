1a4be20d56bc47863d0559810a97c26a44e3e4e8
andrzejki <pieniek.olek@gmail.com>
2024-07-18 22:24:56 +0200

    rename file

---
+++ ScriptedEvents/Actions/Round/RoundAction.cs
@@ -0,0 +1,62 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+
+    using Exiled.API.Features;
+
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+
+    public class RoundAction : IScriptAction, IHelpInfo
+    {
+        /// <inheritdoc/>
+        public string Name => "ROUND";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.Round;
+
+        /// <inheritdoc/>
+        public string Description => "Manages the round and its settings.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+            new OptionsArgument("mode", true,
+                new("START", "Starts the round."),
+                new("END", "Ends the round"),
+                new("LOCK", "Locks the round."),
+                new("UNLOCK", "Unlocks the round"),
+                new("RESTART", "Restarts the round.")),
+        };
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script scr)
+        {
+            switch ((string)Arguments[0])
+            {
+                case "START":
+                    Round.Start(); break;
+                case "END":
+                    Round.EndRound(true); break;
+                case "LOCK":
+                    Round.IsLocked = true; break;
+                case "UNLOCK":
+                    Round.IsLocked = false; break;
+                case "RESTART":
+                    Round.Restart(); break;
+            }
+
+            return new(true);
+        }
+    }
+}
