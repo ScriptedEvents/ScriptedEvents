97f2616978ce55e06ae510398ae78377fb3fbafe
andrzejki <pieniek.olek@gmail.com>
2024-08-14 11:05:50 +0200

    add TPPLAYER action

---
+++ ScriptedEvents/Actions/Teleportation/TpPlayerAction.cs
@@ -0,0 +1,56 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+
+    using Exiled.API.Features;
+    using Exiled.API.Features.Roles;
+
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+
+    using UnityEngine;
+
+    public class TpPlayerAction : IScriptAction, IHelpInfo
+    {
+        /// <inheritdoc/>
+        public string Name => "TPPLAYER";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.Teleportation;
+
+        /// <inheritdoc/>
+        public string Description => "Teleports players to another (only one!) player.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+            new Argument("players", typeof(PlayerCollection), "The players to teleport", true),
+            new Argument("targetPlayer", typeof(Player), "The player to teleport to.", true),
+        };
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            PlayerCollection players = (PlayerCollection)Arguments[0];
+            Player targetPlayer = (Player)Arguments[1];
+
+            foreach (Player ply in players)
+            {
+                if (ply.Role is not FpcRole || !ply.IsConnected) continue;
+                ply.Teleport(targetPlayer.Position);
+            }
+
+            return new(true);
+        }
+    }
+}
