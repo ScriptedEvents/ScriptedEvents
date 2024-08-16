3444f30aa79f7f50e1de664ae3c89b7e2814c949
andrzejki <pieniek.olek@gmail.com>
2024-08-16 13:39:41 +0200

    Create DebugExtractAction.cs

---
+++ ScriptedEvents/Actions/DebugActions/DebugExtractAction.cs
@@ -0,0 +1,36 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+    using System.Linq;
+
+    using Exiled.API.Features;
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+
+    public class DebugExtractAction : IScriptAction, IHiddenAction
+    {
+        /// <inheritdoc/>
+        public string Name => "DEBUGEXTRACT";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.Debug;
+
+        public Argument[] ExpectedArguments => Array.Empty<Argument>();
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            return new(true, variablesToRet: new[] { (object)Player.List.ToArray(), (object)"10" });
+        }
+    }
+}
