3e4b9f5422e3e18b314baf0588725e0eb97cc3a0
andrzejki <pieniek.olek@gmail.com>
2024-08-19 11:39:05 +0200

    Update IHelpInfo.cs

--- ScriptedEvents/API/Interfaces/IHelpInfo.cs
+++ ScriptedEvents/API/Interfaces/IHelpInfo.cs
@@ -1,7 +1,5 @@
 ﻿namespace ScriptedEvents.API.Interfaces
 {
-    using ScriptedEvents.Structures;
-
     /// <summary>
     /// Signifies that an action can be used in the "HELP" action to get information about it.
     /// </summary>
