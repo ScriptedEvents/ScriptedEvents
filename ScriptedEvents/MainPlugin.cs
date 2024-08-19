c90fe585f79459022907559fcede70bfd7c03e42
andrzejki <pieniek.olek@gmail.com>
2024-08-19 12:29:21 +0200

    Update MainPlugin.cs

--- ScriptedEvents/MainPlugin.cs
+++ ScriptedEvents/MainPlugin.cs
@@ -22,7 +22,7 @@
         /// <summary>
         /// Whether or not this build of the plugin is experimental.
         /// </summary>
-        public const bool IsExperimental = false;
+        public const bool IsExperimental = true;
 
         /// <summary>
         /// Gets a link to the Scripted Events Discord server.
