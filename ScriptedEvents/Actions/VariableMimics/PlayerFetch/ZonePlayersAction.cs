3635e8636a0ae8d3ffa3248eb27ce3afe926b779
andrzejki <pieniek.olek@gmail.com>
2024-09-07 12:34:21 +0200

    add ZONEPLAYERS action

---
+++ ScriptedEvents/Actions/VariableMimics/PlayerFetch/ZonePlayersAction.cs
@@ -0,0 +1,44 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+    using System.Linq;
+
+    using Exiled.API.Enums;
+    using Exiled.API.Features;
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+
+    public class ZonePlayersAction : IScriptAction, IHelpInfo, IMimicsVariableAction
+    {
+        /// <inheritdoc/>
+        public string Name => "ZONEPLAYERS";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.Math;
+
+        /// <inheritdoc/>
+        public string Description => "Returns the players in the specified zone.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+             new Argument("zone", typeof(ZoneType), "The zone to get the players from.", true),
+        };
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            return new(true, variablesToRet: new[] { Player.List.Where(ply => ply.Zone == ((ZoneType)Arguments[0])).ToArray() });
+        }
+    }
+}
