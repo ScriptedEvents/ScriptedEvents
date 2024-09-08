853bb38393e236a10d04c48cb3ae3d25de681cba
andrzejki <pieniek.olek@gmail.com>
2024-09-08 14:21:44 +0200

    add SERVERINFO action

---
+++ ScriptedEvents/Actions/VariableMimics/AllInOne/ServerInfoAction.cs
@@ -0,0 +1,55 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+
+    using Exiled.API.Features;
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Extensions;
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+
+    public class ServerInfoAction : IScriptAction, IHelpInfo, IMimicsVariableAction
+    {
+        /// <inheritdoc/>
+        public string Name => "SERVERINFO";
+
+        /// <inheritdoc/>
+        public string Description => "All-in-one action for getting server related information.";
+
+        public Argument[] ExpectedArguments => new[]
+        {
+            new OptionsArgument("mode", true,
+                new("IP", "Returns the IP of the server."),
+                new("PORT", "Returns the port of the server."),
+                new("NAME", "Returns the name of the server."),
+                new("MAXPLAYERS", "Returns the maximum amount of players the server is set to."),
+                new("TPS", "Returns the amount of ticks per second the server is running on.")),
+        };
+
+        public string[] RawArguments { get; set; }
+
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.AllInOneInfo;
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            string ret = Arguments[0].ToUpper() switch
+            {
+                "IP" => Server.IpAddress,
+                "PORT" => Server.Port.ToString(),
+                "NAME" => Server.Name,
+                "MAXPLAYERS" => Server.MaxPlayerCount.ToString(),
+                "TPS" => Server.Tps.ToString(),
+                _ => throw new ArgumentException("No mode provided")
+            };
+
+            return new(true, variablesToRet: new[] { ret });
+        }
+    }
+}
