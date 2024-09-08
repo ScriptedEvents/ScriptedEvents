e3929a7e14aef9478b31cd25d8138871cbb088d0
andrzejki <pieniek.olek@gmail.com>
2024-09-08 13:00:27 +0200

    move wave variables to an action

---
+++ ScriptedEvents/Actions/VariableMimics/AllInOne/WaveInfoAction.cs
@@ -0,0 +1,69 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+
+    using Exiled.API.Features;
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Extensions;
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+
+    public class WaveInfoAction : IScriptAction, IHelpInfo, IMimicsVariableAction
+    {
+        /// <inheritdoc/>
+        public string Name => "WAVEINFO";
+
+        /// <inheritdoc/>
+        public string Description => "All-in-one action for getting wave related information.";
+
+        public Argument[] ExpectedArguments => new[]
+        {
+            new OptionsArgument("mode", true,
+                new("NEXTTEAM", "Returns the next SpawnableTeamType to spawn."),
+                new("LASTTEAM", "Returns the last spawned SpawnableTeamType."),
+                new("LASTUNIT", "Returns the last spawned SpawnableTeamType UNIT."),
+                new("TOTALWAVES", "Returns the amount of spawn waves which have occured."),
+                new("NTFTICKETS", "Returns the current NTF tickets."),
+                new("CHAOSTICKETS", "Returns the current CI tickets."),
+                new("TIMEUNTILNEW", "Returns the amount of seconds remaining until the new spawn wave."),
+                new("TIMESINCELAST", "Returns the amount of seconds since the last spawn wave."),
+                new("RESPAWNEDPLAYERS", "Returns the players which have spawned with the last spawn wave.")),
+        };
+
+        public string[] RawArguments { get; set; }
+
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.MapInfo;
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            string mode = Arguments[0].ToUpper();
+
+            if (mode is "RESPAWNEDPLAYERS")
+            {
+                return new(true, variablesToRet: new[] { MainPlugin.Handlers.RecentlyRespawned });
+            }
+
+            string ret = mode switch
+            {
+                "NEXTTEAM" => Respawn.NextKnownTeam.ToString(),
+                "LASTTEAM" => MainPlugin.Handlers.MostRecentSpawn.ToString(),
+                "LASTUNIT" => MainPlugin.Handlers.MostRecentSpawnUnit.ToString(),
+                "TOTALWAVES" => MainPlugin.Handlers.RespawnWaves.ToString(),
+                "NTFTICKETS" => Respawn.NtfTickets.ToString(),
+                "CHAOSTICKETS" => Respawn.ChaosTickets.ToString(),
+                "TIMEUNTILNEXT" => Respawn.TimeUntilSpawnWave.TotalSeconds.ToString(),
+                "TIMESINCELAST" => MainPlugin.Handlers.TimeSinceWave.TotalSeconds.ToString(),
+                _ => throw new ArgumentException(),
+            };
+
+            return new(true, variablesToRet: new[] { ret });
+        }
+    }
+}
