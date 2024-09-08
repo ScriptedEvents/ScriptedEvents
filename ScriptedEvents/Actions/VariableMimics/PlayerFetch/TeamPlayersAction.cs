b00bc41380285a1366cff61ae16e146a6e8392c7
andrzejki <pieniek.olek@gmail.com>
2024-09-08 17:10:49 +0200

    add TEAMPLAYERS action

---
+++ ScriptedEvents/Actions/VariableMimics/PlayerFetch/TeamPlayersAction.cs
@@ -0,0 +1,44 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+    using System.Linq;
+
+    using Exiled.API.Features;
+    using PlayerRoles;
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+
+    public class TeamPlayersAction : IScriptAction, IHelpInfo, IMimicsVariableAction
+    {
+        /// <inheritdoc/>
+        public string Name => "TEAMPLAYERS";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.PlayerFetch;
+
+        /// <inheritdoc/>
+        public string Description => "Returns the players from the specified team.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+             new Argument("team", typeof(Team), "The team which the players should be fetched by.", true),
+        };
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            return new(true, variablesToRet: new[] { Player.List.Where(ply => ply.Role.Team == ((Team)Arguments[0])).ToArray() });
+        }
+    }
+}
