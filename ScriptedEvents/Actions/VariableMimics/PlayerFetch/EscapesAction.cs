a1c52fe6b6b5995580792f9a7c94ec35e969273f
andrzejki <pieniek.olek@gmail.com>
2024-09-09 14:38:10 +0200

    Create EscapesAction.cs

---
+++ ScriptedEvents/Actions/VariableMimics/PlayerFetch/EscapesAction.cs
@@ -0,0 +1,56 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+    using System.Linq;
+
+    using Exiled.API.Features;
+    using PlayerRoles;
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Extensions;
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+
+    public class EscapesAction : IScriptAction, IHelpInfo, IMimicsVariableAction
+    {
+        /// <inheritdoc/>
+        public string Name => "ESCAPES";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.PlayerFetch;
+
+        /// <inheritdoc/>
+        public string Description => "Returns players which have escaped the facility.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+             new OptionsArgument("mode", true,
+                    new("ALL", "Scientists and ClassDs which have escaped."),
+                    new("SCIENTISTS", "Scientists which have escaped."),
+                    new("CLASSD", "ClassDs which have escaped.")),
+        };
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            Player[] ret = Arguments[0].ToUpper() switch
+            {
+                "ALL" => MainPlugin.Handlers.Escapes[RoleTypeId.ClassD].Union(MainPlugin.Handlers.Escapes[RoleTypeId.Scientist]).ToArray(),
+                "SCIENTISTS" => MainPlugin.Handlers.Escapes[RoleTypeId.Scientist].ToArray(),
+                "CLASSD" => MainPlugin.Handlers.Escapes[RoleTypeId.ClassD].ToArray(),
+                _ => throw new ArgumentException()
+            };
+
+            return new(true, variablesToRet: new[] { ret });
+        }
+    }
+}
