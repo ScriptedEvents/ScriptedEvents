796efc077312dc213a8e5dc0e5522c6203a230ff
andrzejki <pieniek.olek@gmail.com>
2024-09-10 15:41:01 +0200

    Create KillsAction.cs

---
+++ ScriptedEvents/Actions/VariableMimics/Misc/KillsAction.cs
@@ -0,0 +1,67 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+    using System.Linq;
+
+    using Exiled.API.Features;
+    using PlayerRoles;
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Extensions;
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+
+    public class KillsAction : IScriptAction, IHelpInfo, IMimicsVariableAction
+    {
+        /// <inheritdoc/>
+        public string Name => "KILLS";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.Misc;
+
+        /// <inheritdoc/>
+        public string Description => "Returns the amount of kills, the amount of kills per-role or per-team.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+             new Argument("role", typeof(RoleTypeIdOrTeam), "The role or team to filter by. Optional.", false),
+        };
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            if (Arguments.Length < 1)
+                return new(true, variablesToRet: new[] { MainPlugin.Handlers.Kills.Count.ToString() });
+
+            if (Arguments[0] is RoleTypeId rt)
+            {
+                if (MainPlugin.Handlers.Kills.TryGetValue(rt, out int amt))
+                    return new(true, variablesToRet: new[] { amt.ToString() });
+                else
+                    return new(true, variablesToRet: new[] { 0.ToString() });
+            }
+            else if (Arguments[0] is Team team)
+            {
+                int total = 0;
+                foreach (var kills in MainPlugin.Handlers.Kills)
+                {
+                    if (kills.Key.GetTeam() == team)
+                        total += kills.Value;
+                }
+
+                return new(true, variablesToRet: new[] { total.ToString() });
+            }
+
+            return new(false, "ARGPROC failed. Report this error to SE developers as soon as possible!");
+        }
+    }
+}
