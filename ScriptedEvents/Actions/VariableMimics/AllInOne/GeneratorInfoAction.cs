778681a0b081d8cccec41b3c3d87902987ae7039
andrzejki <pieniek.olek@gmail.com>
2024-09-11 20:36:21 +0200

    Create GeneratorInfoAction.cs

---
+++ ScriptedEvents/Actions/VariableMimics/AllInOne/GeneratorInfoAction.cs
@@ -0,0 +1,61 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+    using System.Linq;
+
+    using Exiled.API.Enums;
+    using Exiled.API.Features;
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Extensions;
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+
+    /// <inheritdoc/>
+    public class GeneratorInfoAction : IScriptAction, IHelpInfo, IMimicsVariableAction
+    {
+        /// <inheritdoc/>
+        public string Name => "GENERATORINFO";
+
+        /// <inheritdoc/>
+        public string Description => "All-in-one action for getting generator related information.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+            new OptionsArgument("mode", true,
+                new("ENGAGED", "Returns the amount of engaged generators."),
+                new("ACTIVATING", "Returns the amount of activating generators."),
+                new("UNLOCKED", "Returns the amount of unlocked generators."),
+                new("OPENED", "Returns the amount of open generators."),
+                new("CLOSED", "Returns the amount of closed generators.")),
+        };
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.AllInOneInfo;
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            int ret = Arguments[0].ToUpper() switch
+            {
+                "ENGAGED" => Generator.Get(GeneratorState.Engaged).Count(),
+                "ACTIVATING" => Generator.Get(GeneratorState.Activating).Count(),
+                "UNLOCKED" => Generator.Get(GeneratorState.Unlocked).Count(),
+                "OPENED" => Generator.Get(GeneratorState.Open).Count(),
+                "CLOSED" => Generator.Get(gen => gen.IsOpen is false).Count(),
+                _ => throw new ArgumentException()
+            };
+
+            return new(true, variablesToRet: new[] { ret.ToString() });
+        }
+    }
+}
