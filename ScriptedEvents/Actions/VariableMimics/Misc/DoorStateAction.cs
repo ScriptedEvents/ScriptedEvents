38fed3e5988eca90578117438caeec61934bea43
andrzejki <pieniek.olek@gmail.com>
2024-09-11 20:52:26 +0200

    Create DoorStateAction.cs

---
+++ ScriptedEvents/Actions/VariableMimics/Misc/DoorStateAction.cs
@@ -0,0 +1,50 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+    using Exiled.API.Enums;
+    using Exiled.API.Features.Doors;
+    using PlayerRoles;
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Features;
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+
+    /// <inheritdoc/>
+    public class DoorStateAction : IScriptAction, IHelpInfo, IMimicsVariableAction
+    {
+        /// <inheritdoc/>
+        public string Name => "DOORSTATE";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.Misc;
+
+        /// <inheritdoc/>
+        public string Description => "Returns the state of a door (either 'OPEN' or 'CLOSED').";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+             new Argument("door", typeof(DoorType), "The door to get the state of.", true),
+        };
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            DoorType dt = (DoorType)Arguments[0];
+            Door d = Door.Get(dt);
+
+            return d is null
+                ? throw new ArgumentException(ErrorGen.Get(ErrorCode.InvalidEnumGeneric, dt.ToString(), nameof(DoorType)))
+                : new(true, variablesToRet: new[] { (d.IsOpen ? "OPEN" : "CLOSED") });
+        }
+    }
+}
