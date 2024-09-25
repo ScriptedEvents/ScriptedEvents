e7a12d7abb391664d83e3a3797a5987db8a6389a
andrzejki <pieniek.olek@gmail.com>
2024-09-25 18:27:17 +0200

    make display a action from variable

---
+++ ScriptedEvents/Actions/VariableMimics/Misc/DisplayPlayerAction.cs
@@ -0,0 +1,49 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+    using System.Linq;
+
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+
+    /// <inheritdoc/>
+    public class DisplayPlayerAction : IScriptAction, IHelpInfo, IMimicsVariableAction
+    {
+        /// <inheritdoc/>
+        public string Name => "DISPLAYPLR";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.Misc;
+
+        /// <inheritdoc/>
+        public string Description => "Returns a message where all provided players are formatted like 'playerName1, playerName2, etc'. Returns 'NONE' if player reference holds 0 players.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+             new Argument("players", typeof(PlayerCollection), "The players to display.", true),
+        };
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            PlayerCollection players = (PlayerCollection)Arguments[0];
+            if (players.Length == 0)
+            {
+                return new(true, variablesToRet: new[] { "NONE" });
+            }
+
+            return new(true, variablesToRet: new[] { string.Join(", ", players.Select(p => p.Nickname)) });
+        }
+    }
+}
