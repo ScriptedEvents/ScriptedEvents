4c09ef2c3f2cefb9d6e6972b99bde00df02e6c1d
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2024-10-12 12:01:06 +0200

    add COPY action

---
+++ ScriptedEvents/Actions/VariableActions/CopyAction.cs
@@ -0,0 +1,61 @@
+﻿using Exiled.API.Features;
+
+namespace ScriptedEvents.Actions
+{
+    using System;
+    using System.Linq;
+
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Extensions;
+
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+    using ScriptedEvents.Variables.Interfaces;
+
+    public class CopyAction : IScriptAction, IHelpInfo
+    {
+        /// <inheritdoc/>
+        public string Name => "COPY";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.Variable;
+
+        /// <inheritdoc/>
+        public string Description => "Returns the provided variable value.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+            new Argument("variable", typeof(IVariable), "The variable to copy.", false),
+        };
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            object toRet;
+
+            switch ((IVariable)Arguments[0])
+            {
+                case IPlayerVariable plrVar:
+                    toRet = plrVar.Players.ToArray();
+                    break;
+                case ILiteralVariable lvVar:
+                    toRet = lvVar.Value;
+                    break;
+                default:
+                    throw new ArgumentException("Variable is not a valid variable type");
+            }
+
+            return new(true, variablesToRet: new[] { toRet });
+        }
+    }
+}
