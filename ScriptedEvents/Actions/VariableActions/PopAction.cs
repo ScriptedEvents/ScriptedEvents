5fac5d764d26a9ce4be0fafb9fd394b44143d3e2
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2024-10-12 12:01:13 +0200

    add POP action

---
+++ ScriptedEvents/Actions/VariableActions/PopAction.cs
@@ -0,0 +1,61 @@
+﻿namespace ScriptedEvents.Actions
+{
+    using System;
+    using System.Linq;
+
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Extensions;
+
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+    using ScriptedEvents.Variables.Interfaces;
+
+    public class PopAction : IScriptAction, IHelpInfo
+    {
+        /// <inheritdoc/>
+        public string Name => "POP";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.Variable;
+
+        /// <inheritdoc/>
+        public string Description => "Deletes the provided local variable and returns its value.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+            new Argument("variable", typeof(IVariable), "The variable to delete.", false),
+        };
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            object toRet;
+
+            switch ((IVariable)Arguments[0])
+            {
+                case IPlayerVariable plrVar:
+                    script.RemoveVariable(plrVar);
+                    toRet = plrVar.GetPlayers().ToArray();
+                    break;
+                case ILiteralVariable lvVar:
+                    script.RemoveVariable(lvVar);
+                    toRet = lvVar.Value;
+                    break;
+                default:
+                    throw new ArgumentException("Variable is not a valid variable type");
+            }
+
+            return new(true, variablesToRet: new[] { toRet });
+        }
+    }
+}
