1e5f28842535e8564996fb7a0c8beb168d41ace5
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2024-10-12 12:01:19 +0200

    add JOIN action

---
+++ ScriptedEvents/Actions/VariableMimicingActions/PlayerVariableManipulation/JoinAction.cs
@@ -0,0 +1,51 @@
+﻿namespace ScriptedEvents.Actions.VariableMimicingActions.PlayerVariableManipulation
+{
+    using System;
+
+    using ScriptedEvents.API.Enums;
+    using ScriptedEvents.API.Interfaces;
+    using ScriptedEvents.Structures;
+
+    public class JoinAction : IScriptAction, IHelpInfo, IMimicsVariableAction
+    {
+        /// <inheritdoc/>
+        public string Name => "JOIN";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.PlayerVariableManipualtion;
+
+        /// <inheritdoc/>
+        public string Description => "Returns a player value where players from the first and second reference are joined together.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+            new Argument("players1", typeof(PlayerCollection), "First player reference.", true),
+            new Argument("players2", typeof(PlayerCollection), "Second player reference.", true),
+        };
+
+        /// <inheritdoc/>
+        public ActionResponse Execute(Script script)
+        {
+            var players = ((PlayerCollection)Arguments[0]).GetInnerList();
+            foreach (var plr in (PlayerCollection)Arguments[1])
+            {
+                if (!players.Contains(plr))
+                {
+                    players.Add(plr);
+                }
+            }
+
+            return new(true, variablesToRet: new object[] { players.ToArray() });
+        }
+    }
+}
