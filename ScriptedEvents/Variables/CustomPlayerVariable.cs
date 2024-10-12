6cf59e79032a64fe653caf9e8402c7e80e928c09
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2024-10-12 12:13:47 +0200

    refactor CustomPlayerVariable

--- ScriptedEvents/Variables/CustomPlayerVariable.cs
+++ ScriptedEvents/Variables/CustomPlayerVariable.cs
@@ -1,12 +1,15 @@
 ﻿namespace ScriptedEvents.Variables
 {
     using System.Collections.Generic;
+    using System.Linq;
 
     using Exiled.API.Features;
     using ScriptedEvents.Variables.Interfaces;
 
     public class CustomPlayerVariable : IPlayerVariable
     {
+        private readonly IEnumerable<Player> _players;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="CustomPlayerVariable"/> class.
         /// </summary>
@@ -20,11 +23,11 @@
         /// <param name="name">The name of the variable.</param>
         /// <param name="description">The description of the variable.</param>
         /// <param name="value">The players to add to the variable.</param>
-        public CustomPlayerVariable(string name, string description, List<Player> value)
+        public CustomPlayerVariable(string name, string description, IEnumerable<Player> value)
         {
             Name = name;
             Description = description;
-            PlayerList = value;
+            _players = value;
         }
 
         /// <inheritdoc/>
@@ -33,35 +36,8 @@
         /// <inheritdoc/>
         public string Description { get; }
 
-        /// <inheritdoc/>
-        public IEnumerable<Player> Players => PlayerList;
+        IEnumerable<Player> IPlayerVariable.Players => _players;
 
-        private List<Player> PlayerList { get; }
-
-        /// <summary>
-        /// Adds a range of players to the variable. Will ignore duplicates.
-        /// </summary>
-        /// <param name="player">The players to add.</param>
-        public void Add(params Player[] player)
-        {
-            foreach (Player plr in player)
-            {
-                if (!PlayerList.Contains(plr))
-                    PlayerList.Add(plr);
-            }
-        }
-
-        /// <summary>
-        /// Removes a range of players from the variable. Will ignore duplicates.
-        /// </summary>
-        /// <param name="player">The players to remove.</param>
-        public void Remove(params Player[] player)
-        {
-            foreach (Player plr in player)
-            {
-                if (PlayerList.Contains(plr))
-                    PlayerList.Remove(plr);
-            }
-        }
+        public IEnumerable<Player> GetPlayers() => _players.Where(plr => plr is not null);
     }
 }
