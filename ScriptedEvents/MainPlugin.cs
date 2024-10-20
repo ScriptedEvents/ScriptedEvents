f283c7324d2064ebe7761ec95db3b6b9f707137c
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2024-10-20 13:20:23 +0200

    Update MainPlugin.cs

--- ScriptedEvents/MainPlugin.cs
+++ ScriptedEvents/MainPlugin.cs
@@ -76,7 +76,7 @@
         public override string Author => "Thunder and Elektryk_Andrzej";
 
         /// <inheritdoc/>
-        public override Version Version => new(3, 1, 7);
+        public override Version Version => new(3, 1, 9);
 
         /// <inheritdoc/>
         public override Version RequiredExiledVersion => new(8, 7, 0);
