4ac00dd2a32a3ab547dd0f636f187ff63b24d2d6
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2024-10-29 18:36:37 +0100

    Update SuggestedOptionsArgument.cs

--- ScriptedEvents/Structures/SuggestedOptionsArgument.cs
+++ ScriptedEvents/Structures/SuggestedOptionsArgument.cs
@@ -1,8 +1,10 @@
 ﻿namespace ScriptedEvents.Structures
 {
+    using ScriptedEvents.Interfaces;
+
     public class SuggestedOptionsArgument : OptionsArgument
     {
-        public SuggestedOptionsArgument(string argumentName, bool required, params Option[] options)
+        public SuggestedOptionsArgument(string argumentName, bool required, params IOption[] options)
             : base(argumentName, required, options)
         {
         }
