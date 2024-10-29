119e071ac80ea686a3f6379178795a8120f7b59e
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2024-10-29 11:06:41 +0100

    Create TypeExtensions.cs

---
+++ ScriptedEvents/API/Extensions/TypeExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace ScriptedEvents.API.Extensions
+{
+    using System;
+
+    /// <summary>
+    /// Contains useful extensions.
+    /// </summary>
+    public static class TypeExtensions
+    {
+        public static T[] ToNotNullArray<T>(this T? input)
+        {
+            return input is null ? new T[] { } : new[] { input };
+        }
+
+        public static object ToNotNullArrayObject<T>(this T input)
+        {
+            return ToNotNullArray(input);
+        }
+
+        public static object ToStringObject<T>(this T input) =>
+            input?.ToString()
+            ?? throw new Exception("ToStringObject method got null!!!");
+    }
+}
