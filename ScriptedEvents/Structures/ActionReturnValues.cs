73161a0072552a6192917c970b607c81c0fbbd8a
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2024-10-29 12:20:24 +0100

    create ActionReturnValues class

---
+++ ScriptedEvents/Structures/ActionReturnValues.cs
@@ -0,0 +1,27 @@
+﻿namespace ScriptedEvents.Structures
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Exiled.API.Features;
+
+    public class ActionReturnValues
+    {
+        public ActionReturnValues(string value)
+        {
+            Values = new[] { value };
+        }
+
+        public ActionReturnValues(Player player)
+        {
+            Values = new[] { new[] { player } };
+        }
+
+        public ActionReturnValues(IEnumerable<Player> players)
+        {
+            Values = new[] { players as Player[] ?? players.ToArray() };
+        }
+
+        public object[] Values { get; set; }
+    }
+}
