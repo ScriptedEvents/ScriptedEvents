5813be3a6c7f2e2a5192256d2b664f4348fdd938
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2024-10-29 12:26:28 +0100

    Update ItemInfoAction.cs

--- ScriptedEvents/Actions/AllInOne/ItemInfoAction.cs
+++ ScriptedEvents/Actions/AllInOne/ItemInfoAction.cs
@@ -1,34 +1,33 @@
-﻿namespace ScriptedEvents.Actions.ItemActions
+﻿namespace ScriptedEvents.Actions.AllInOne
 {
     using System;
 
     using Exiled.API.Features;
-    using Exiled.API.Features.Items;
     using ScriptedEvents.API.Enums;
     using ScriptedEvents.API.Extensions;
     using ScriptedEvents.API.Interfaces;
     using ScriptedEvents.Structures;
 
-    /// <inheritdoc/>
+    using Item = Exiled.API.Features.Items.Item;
+
     public class ItemInfoAction : IScriptAction, IHelpInfo, IMimicsVariableAction
     {
         /// <inheritdoc/>
-        public string Name => "ITEMINFO";
+        public string Name => "ItemInfo";
 
         /// <inheritdoc/>
-        public string Description => "All-in-one action for getting item related information from the provided item id.";
+        public string Description => "All-in-one action for getting item related information from the provided item object.";
 
         /// <inheritdoc/>
         public Argument[] ExpectedArguments => new[]
         {
              new OptionsArgument("mode", true,
-                    new("IsValid", "Is provided item id a valid item."),
                     new("IsCarried", "Is item is in owner's inventory."),
                     new("Scale", "Item's scale."),
                     new("Weight", "Item's weight."),
                     new("Owner", "Item's owner."),
                     new("Type", "Item's type.")),
-             new Argument("itemId", typeof(ushort), "The item id to use.", true),
+             new Argument("item", typeof(Item), "The item to use.", true),
         };
 
         /// <inheritdoc/>
@@ -47,29 +46,17 @@
         public ActionResponse Execute(Script script)
         {
             string mode = Arguments[0].ToUpper();
-            ushort id = (ushort)Arguments[1];
-
-            Item item = Item.Get(id);
-
-            if (item is null)
-            {
-                if (mode == "ISVALID")
-                    return new(true, variablesToRet: new[] { "FALSE" });
-
-                return new(
-                    false,
-                    new ErrorTrace(new ErrorInfo("Item reference error", $"Provided item ID '{id}' is not valid. Use the 'ISVALID' mode to verify the ID before accessing item info.", "ITEMINFO action")));
-            }
+            Item item = (Item)Arguments[1];
 
             if (mode is "OWNER")
             {
                 Player plr = item.Owner;
                 if (plr is null)
                 {
-                    return new(true);
+                    return new(true, new(Array.Empty<Player>()));
                 }
 
-                return new(true, variablesToRet: new[] { plr });
+                return new(true, new(plr));
             }
 
             string ret = mode switch
@@ -82,7 +69,7 @@
                 _ => throw new ArgumentException()
             };
 
-            return new(true, variablesToRet: new[] { ret });
+            return new(true, new(ret));
         }
     }
 }
