4c2ade5d4ba9e41e09254e8240bac7d1ebe92ee9
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2024-11-05 18:17:24 +0100

    add cuffer variable

--- ScriptedEvents/Variables/MiscVariables.cs
+++ ScriptedEvents/Variables/MiscVariables.cs
@@ -1,5 +1,10 @@
 ﻿namespace ScriptedEvents.Variables.Misc
 {
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Exiled.API.Features;
+
 #pragma warning disable SA1402 // File may only contain a single type
     using ScriptedEvents.API.Extensions;
     using ScriptedEvents.API.Features;
@@ -16,6 +21,7 @@
         {
             new Storage(),
             new Log(),
+            new Cuffer(),
         };
     }
 
@@ -50,6 +56,39 @@
                 IConditionVariable variable = (IConditionVariable)Arguments[0];
 
                 return $"{variable.Name.Trim('{', '}')} = {variable.String()}";
+            }
+        }
+    }
+
+    public class Cuffer : IPlayerVariable, IArgumentVariable, INeedSourceVariable
+    {
+        /// <inheritdoc/>
+        public string Name => "{CUFFER}";
+
+        /// <inheritdoc/>
+        public string Description => "Gets the player who has cuffed a specified player, if specified player is not cuffed or the specified player was not cuffed by a different player, value will be an empty player variable.";
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public Script Source { get; set; }
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+            new Argument("player", typeof(Player), "The player which is cuffed.", true),
+        };
+
+        public IEnumerable<Player> Players
+        {
+            get
+            {
+                var plr = ((Player)Arguments[0]).Cuffer;
+                return plr is null ? Enumerable.Empty<Player>() : new[] { plr };
             }
         }
     }
