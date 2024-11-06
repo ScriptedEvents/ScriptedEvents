e576ef340e847da14e2310cc8fb5b2001c193d18
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2024-11-06 21:50:54 +0100

    Update HitMarker.cs

--- ScriptedEvents/DemoScripts/HitMarker.cs
+++ ScriptedEvents/DemoScripts/HitMarker.cs
@@ -15,7 +15,7 @@ HINT {EVATTACKER} 5 <size=28>[ Subject <color=red>{GET:EVPLAYER:DPNAME}</color> 
 The opposite would be {EVATTACKER}, who caused said event
 {GET} collects a user's data in a Player Variable
 !-- EVENT uses an EXILED event to fire, which can be viewed below:
-https://github.com/Exiled-Team/EXILED/tree/master/Exiled.Events/EventArgs
+https://github.com/ExMod-Team/EXILED/tree/master/EXILED/Exiled.Events/Handlers
 
 Remember to use [shelp list/listvar] in your server console for any help,
 Or don't hesitate to reach out to the support team in the SE Discord!
