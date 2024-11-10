59d051d25941201f016ab54be9651faad49afce1
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2024-11-10 16:30:42 +0100

    add ImpossibleException

---
+++ ScriptedEvents/API/Features/Exceptions/ImpossibleException.cs
@@ -0,0 +1,17 @@
+﻿namespace ScriptedEvents.API.Features.Exceptions
+{
+    using System;
+
+    /// <summary>
+    /// An expection that should be impossible to trigger.
+    /// </summary>
+    public class ImpossibleException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ImpossibleException"/> class.
+        /// </summary>
+        public ImpossibleException()
+        {
+        }
+    }
+}
