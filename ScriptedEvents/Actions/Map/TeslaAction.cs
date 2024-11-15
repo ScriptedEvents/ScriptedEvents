353a390189c3ae16fbc7ac658384cbdbd864fc0c
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2024-11-15 18:50:59 +0100

    make TESLA action unusable

--- ScriptedEvents/Actions/Map/TeslaAction.cs
+++ ScriptedEvents/Actions/Map/TeslaAction.cs
@@ -1,30 +1,15 @@
-﻿using ScriptedEvents.Enums;
+﻿using System;
+using ScriptedEvents.Enums;
 using ScriptedEvents.Interfaces;
+using ScriptedEvents.Structures;
 
-namespace ScriptedEvents.Actions.MapActions
+namespace ScriptedEvents.Actions.Map
 {
-    using System;
-    using System.Collections.Generic;
-
-    using Exiled.API.Features;
-
-    using MEC;
-
-    using PlayerRoles;
-
-    using ScriptedEvents.Actions.Samples.Interfaces;
-    using ScriptedEvents.Actions.Samples.Providers;
-    using ScriptedEvents.API.Extensions;
-    using ScriptedEvents.API.Features;
-    using ScriptedEvents.Structures;
-
-    using Tesla = Exiled.API.Features.TeslaGate;
-
     // Todo: Needs reworked entirely
     public class TeslaAction : IScriptAction, IHelpInfo
     {
         /// <inheritdoc/>
-        public string Name => "TESLA";
+        public string Name => "Tesla";
 
         /// <inheritdoc/>
         public string[] Aliases => Array.Empty<string>();
@@ -33,7 +18,7 @@ namespace ScriptedEvents.Actions.MapActions
         public string[] RawArguments { get; set; }
 
         /// <inheritdoc/>
-        public object[] Arguments { get; set; }
+        public object?[] Arguments { get; set; }
 
         /// <inheritdoc/>
         public ActionSubgroup Subgroup => ActionSubgroup.Map;
@@ -42,18 +27,20 @@ namespace ScriptedEvents.Actions.MapActions
         public string Description => "Modifies tesla gates.";
 
         /// <inheritdoc/>
-        public Argument[] ExpectedArguments => new[]
-        {
-            new OptionsArgument("mode", true,
-                new("Detect", "Make tesla gates detect specified players."),
-                new("Ignore", "Make tesla gates ignore specified players.")),
-            new Argument("players", typeof(PlayerCollection), "The players to affect.", true),
-        };
+        public Argument[] ExpectedArguments => Array.Empty<Argument>();
 
         /// <inheritdoc/>
         public ActionResponse Execute(Script script)
         {
-            return new(false, "Action has no implementation.");
+            return new(
+                false, 
+                null, 
+                new ErrorInfo(
+                    "Not implemented",
+                    "This action is not implemented.",
+                    "Tesla action"
+                ).ToTrace()
+            );
         }
     }
 }
