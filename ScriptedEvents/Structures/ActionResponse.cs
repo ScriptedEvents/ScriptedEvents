78f61281f58f707438272aff7ab1cf8e7b8207d0
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2024-11-15 18:54:04 +0100

    housekeeping

--- ScriptedEvents/Structures/ActionResponse.cs
+++ ScriptedEvents/Structures/ActionResponse.cs
@@ -9,13 +9,6 @@ namespace ScriptedEvents.Structures
     /// </summary>
     public class ActionResponse
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ActionResponse"/> class.
-        /// </summary>
-        /// <param name="success">Whether or not the execution of the action was successful.</param>
-        /// <param name="errorTrace">The error trace if the action has failed.</param>
-        /// <param name="flags">Flags that control what happens after the execution is complete.</param>
-        /// <param name="variablesToRet">Variables to assign after successful action usage.</param>
         public ActionResponse(bool success, ActionReturnValues? values = null, ErrorTrace? errorTrace = null, ActionFlags flags = ActionFlags.None)
         {
             Success = success;
