d9c63d6c00bcafbef105abf0e286dd7f280e48fb
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2024-11-15 18:49:42 +0100

    Update DoorAction.cs

--- ScriptedEvents/Actions/Map/DoorAction.cs
+++ ScriptedEvents/Actions/Map/DoorAction.cs
@@ -1,20 +1,19 @@
-﻿using ScriptedEvents.Enums;
-using ScriptedEvents.Interfaces;
-
-namespace ScriptedEvents.Actions
+﻿namespace ScriptedEvents.Actions.Map
 {
     using System;
-
     using Exiled.API.Enums;
     using Exiled.API.Features.Doors;
     using Exiled.API.Interfaces;
     using ScriptedEvents.API.Extensions;
+    using ScriptedEvents.API.Features.Exceptions;
+    using ScriptedEvents.Enums;
+    using ScriptedEvents.Interfaces;
     using ScriptedEvents.Structures;
 
     public class DoorAction : IScriptAction, IHelpInfo
     {
         /// <inheritdoc/>
-        public string Name => "DOOR";
+        public string Name => "Door";
 
         /// <inheritdoc/>
         public string[] Aliases => Array.Empty<string>();
@@ -23,60 +22,48 @@ namespace ScriptedEvents.Actions
         public string[] RawArguments { get; set; }
 
         /// <inheritdoc/>
-        public object[] Arguments { get; set; }
+        public object?[] Arguments { get; set; }
 
         /// <inheritdoc/>
         public ActionSubgroup Subgroup => ActionSubgroup.Map;
 
         /// <inheritdoc/>
-        public string Description => "Controls map doors.";
+        public string Description => "Manages facility doors. Keep in mind that some modes might not work with some doors.";
 
         /// <inheritdoc/>
         public Argument[] ExpectedArguments => new[]
         {
             new OptionsArgument("mode", true,
-                new("Lock", "Lock doors."),
-                new("Unlock", "Unlock doors."),
-                new("Open", "Open doors."),
-                new("Close", "Close doors."),
-                new("Destroy", "Permanently destroy doors.")),
+                new Option("Lock", "Lock doors."),
+                new Option("Unlock", "Unlock doors."),
+                new Option("Open", "Open doors."),
+                new Option("Close", "Close doors."),
+                new Option("Destroy", "Permanently destroy doors.")),
             new Argument("doors", typeof(Door[]), "The doors to affect.", true),
         };
 
         /// <inheritdoc/>
         public ActionResponse Execute(Script script)
         {
-            Door[] doors = (Door[])Arguments[1];
+            var doors = (Door[])Arguments[1]!;
 
-            Action<Door> action = null;
-            switch (Arguments[0].ToUpper())
+            Action<Door> action = Arguments[0]!.ToUpper() switch
             {
-                case "OPEN":
-                    action = (door) => door.IsOpen = true;
-                    break;
-                case "CLOSE":
-                    action = (door) => door.IsOpen = false;
-                    break;
-                case "LOCK":
-                    action = (door) =>
-                    {
-                        if (door.IsLocked)
-                            return;
+                "OPEN" => (door) => door.IsOpen = true,
+                "CLOSE" => (door) => door.IsOpen = false,
+                "LOCK" => (door) =>
+                {
+                    if (door.IsLocked) return;
 
-                        door.ChangeLock(DoorLockType.AdminCommand);
-                    };
-                    break;
-                case "UNLOCK":
-                    action = (door) => door.Unlock();
-                    break;
-                case "DESTROY":
-                    action = (door) =>
-                    {
-                        if (door is IDamageableDoor breaker && !breaker.IsDestroyed)
-                            breaker.Break();
-                    };
-                    break;
-            }
+                    door.ChangeLock(DoorLockType.AdminCommand);
+                },
+                "UNLOCK" => (door) => door.Unlock(),
+                "DESTROY" => (door) =>
+                {
+                    if (door is IDamageableDoor { IsDestroyed: false } breaker) breaker.Break();
+                },
+                _ => throw new ImpossibleException()
+            };
 
             foreach (Door door in doors)
             {
