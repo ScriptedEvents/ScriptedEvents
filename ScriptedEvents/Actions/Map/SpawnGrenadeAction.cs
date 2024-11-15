3a909601ffa47d2db7fda5c51ce41728ce76828a
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2024-11-15 18:50:42 +0100

    Update SpawnGrenadeAction.cs

--- ScriptedEvents/Actions/Map/SpawnGrenadeAction.cs
+++ ScriptedEvents/Actions/Map/SpawnGrenadeAction.cs
@@ -1,28 +1,27 @@
-﻿using ScriptedEvents.Enums;
+﻿using System;
+using System.Linq;
+using Exiled.API.Features.Items;
+using ScriptedEvents.Enums;
 using ScriptedEvents.Interfaces;
+using ScriptedEvents.Structures;
+using ScriptedEvents.Variables.Interfaces;
+using UnityEngine;
 
-namespace ScriptedEvents.Actions.MapActions
+namespace ScriptedEvents.Actions.Map
 {
-    using System.Linq;
-
-    using Exiled.API.Features.Items;
-    using ScriptedEvents.Structures;
-    using ScriptedEvents.Variables.Interfaces;
-    using UnityEngine;
-
     public class SpawnGrenadeAction : IScriptAction, IHelpInfo
     {
         /// <inheritdoc/>
-        public string Name => "GRENADE";
+        public string Name => "SpawnGrenade";
 
         /// <inheritdoc/>
-        public string[] Aliases => new[] { "SPAWNGRENADE" };
+        public string[] Aliases => Array.Empty<string>();
 
         /// <inheritdoc/>
         public string[] RawArguments { get; set; }
 
         /// <inheritdoc/>
-        public object[] Arguments { get; set; }
+        public object?[] Arguments { get; set; }
 
         /// <inheritdoc/>
         public ActionSubgroup Subgroup => ActionSubgroup.Map;
@@ -44,23 +43,20 @@ namespace ScriptedEvents.Actions.MapActions
         /// <inheritdoc/>
         public ActionResponse Execute(Script script)
         {
-            var grenade = (ExplosiveGrenade)Item.Create(ItemType.GrenadeHE);
+            var grenade = (ExplosiveGrenade)Exiled.API.Features.Items.Item.Create(ItemType.GrenadeHE);
             Vector3 pos = new(
-                (float)Arguments[0],
-                (float)Arguments[1],
-                (float)Arguments[2]);
+                (float)Arguments[0]!,
+                (float)Arguments[1]!,
+                (float)Arguments[2]!
+            );
 
-            grenade.FuseTime = (float)Arguments[3];
+            grenade.FuseTime = (float)Arguments[3]!;
             var gren = grenade.SpawnActive(pos);
 
-            if (Arguments.Length > 4)
-            {
-                float scale = (float)Arguments[4];
-                gren.Scale = new(scale, scale, scale);
-            }
-
-            if (Arguments.Length > 5)
-                gren.PreviousOwner = ((IPlayerVariable)Arguments[5]).GetPlayers().FirstOrDefault();
+            float scale = (float?)Arguments[4] ?? 1;
+            gren.Scale = new(scale, scale, scale);
+            
+            gren.PreviousOwner = ((IPlayerVariable?)Arguments[5])?.GetPlayers().FirstOrDefault();
 
             return new(true);
         }
