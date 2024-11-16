445a0f9d4673748f74ecf6fd5c2098d46d5cc939
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2024-11-16 18:57:15 +0100

    remove most HTTP acttions, they need a full rework later

--- ScriptedEvents/Actions/Misc/HttpPostAction.cs
+++ ScriptedEvents/Actions/Misc/HttpPostAction.cs
@@ -1,22 +1,19 @@
+using System;
+using System.Collections.Generic;
+using MEC;
+using ScriptedEvents.API.Extensions;
+using ScriptedEvents.API.Features;
 using ScriptedEvents.Enums;
 using ScriptedEvents.Interfaces;
+using ScriptedEvents.Structures;
+using UnityEngine.Networking;
 
-namespace ScriptedEvents.Actions.MiscActions
+namespace ScriptedEvents.Actions.Misc
 {
-    using System;
-    using System.Collections.Generic;
-
-    using MEC;
-    using ScriptedEvents.API.Extensions;
-    using ScriptedEvents.API.Features;
-    using ScriptedEvents.Structures;
-
-    using UnityEngine.Networking;
-
-    public class HttpPostAction : IHelpInfo, ITimingAction, ILongDescription
+    public class HttpPostAction : IHelpInfo, ITimingAction, ILongDescription, IReturnValueAction
     {
         /// <inheritdoc/>
-        public string Name => "HTTPPOST";
+        public string Name => "HTTPPost";
 
         /// <inheritdoc/>
         public string[] Aliases => Array.Empty<string>();
@@ -25,7 +22,7 @@ namespace ScriptedEvents.Actions.MiscActions
         public string[] RawArguments { get; set; }
 
         /// <inheritdoc/>
-        public object[] Arguments { get; set; }
+        public object?[] Arguments { get; set; }
 
         /// <inheritdoc/>
         public ActionSubgroup Subgroup => ActionSubgroup.Misc;
@@ -34,16 +31,19 @@ namespace ScriptedEvents.Actions.MiscActions
         public string Description => "Sends an HTTP POST request to a website.";
 
         /// <inheritdoc/>
-        public string LongDescription => @"This action pauses the script until the request is complete. Upon completion, three variables will be created:
-- $HTTPSUCCESS - True or false, depending on if the website returned HTTP Code 200.
-- $HTTPCODE - The status code response.
-- $HTTPRESULT - The body of information returned from the website.
+        public string LongDescription => @"This action can either pause until the request is complete or not. This is handled by the 'mode' argument.
 
-These variables are created as per-script variables, meaning they can only be used in the script that created them. If these variables already exist (such as a prior HTTPGET/HTTPPOST execution), they will be overwritten with new values.";
+If 'shouldWait' mode is used, three values will be returned upon completion of the request:
+- a TRUE/FALSE value, depending on if the website returned HTTP Code 200.
+- a number value - The status code response.
+- a string value - The body of information returned from the website.";
 
         /// <inheritdoc/>
         public Argument[] ExpectedArguments => new[]
         {
+            new OptionsArgument("mode", true,
+                new Option("NoWait", "Action will not wait until the request is finished."),
+                new Option("WaitForFinish", "Action will wait until the request is finished and will return values.")),
             new Argument("url", typeof(string), "The URL to send a POST request to.", true),
             new Argument("body", typeof(string), "The body to send (JSON formatting).", true),
         };
@@ -66,12 +66,10 @@ These variables are created as per-script variables, meaning they can only be us
             discordWWW.method = "POST";
             discordWWW.SetRequestHeader("Content-Type", "application/json");
             yield return Timing.WaitUntilDone(discordWWW.SendWebRequest());
-
-            string result;
-            if (discordWWW.responseCode != 200)
-                result = $"ERROR {discordWWW.responseCode}";
-            else
-                result = discordWWW.downloadHandler.text;
+            
+            string result = discordWWW.responseCode != 200 
+                ? $"ERROR {discordWWW.responseCode}" 
+                : discordWWW.downloadHandler.text;
 
             script.AddLiteralVariable("HTTPCODE", discordWWW.responseCode.ToString(), true);
             script.AddLiteralVariable("HTTPSUCCESS", (discordWWW.responseCode == 200).ToString().ToUpper(), true);
