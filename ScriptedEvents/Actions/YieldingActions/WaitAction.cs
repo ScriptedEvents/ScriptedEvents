d22b0f478aa10a5a17243454040fbf8a78e39e90
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2024-11-19 20:08:26 +0100

    rework Yielding actions

--- ScriptedEvents/Actions/YieldingActions/WaitAction.cs
+++ ScriptedEvents/Actions/YieldingActions/WaitAction.cs
@@ -1,21 +1,15 @@
-﻿using ScriptedEvents.Enums;
+﻿using System;
+using MEC;
+using ScriptedEvents.Enums;
 using ScriptedEvents.Interfaces;
+using ScriptedEvents.Structures;
 
-namespace ScriptedEvents.Actions
+namespace ScriptedEvents.Actions.YieldingActions
 {
-    using System;
-    using System.Collections.Generic;
-
-    using MEC;
-    using ScriptedEvents.API.Extensions;
-    using ScriptedEvents.API.Features;
-    using ScriptedEvents.API.Modules;
-    using ScriptedEvents.Structures;
-
     public class WaitAction : ITimingAction, IHelpInfo
     {
         /// <inheritdoc/>
-        public string Name => "WAIT";
+        public string Name => "Wait";
 
         /// <inheritdoc/>
         public string[] Aliases => Array.Empty<string>();
@@ -24,73 +18,25 @@ namespace ScriptedEvents.Actions
         public string[] RawArguments { get; set; }
 
         /// <inheritdoc/>
-        public object[] Arguments { get; set; }
+        public object?[] Arguments { get; set; }
 
         /// <inheritdoc/>
         public ActionSubgroup Subgroup => ActionSubgroup.Yielding;
 
         /// <inheritdoc/>
-        public string Description => "Yields execution of the script.";
+        public string Description => "Yields the script execution for a specified amount of time.";
 
         /// <inheritdoc/>
         public Argument[] ExpectedArguments => new[]
         {
-            new OptionsArgument("mode", true,
-                new("SEC", "'value' argument will expect amout of seconds to wait for."),
-                new("MIL", "'value' argument will expect amout of miliseconds to wait for. "),
-                new("UNTIL", "'value argument will expect a condition. Will yield until said condition evaluates to TRUE.")),
-            new Argument("value", typeof(object), "The value. Math is supported.", true),
+            new Argument("time", typeof(TimeSpan), "The time to wait.", true),
         };
 
         /// <inheritdoc/>
         public float? Execute(Script script, out ActionResponse message)
         {
-            string formula = Arguments.JoinMessage(1);
-
-            if (Arguments[0].ToUpper() == "UNTIL")
-            {
-                string coroutineKey = $"WAITUNTIL_COROUTINE_{DateTime.Now.Ticks}";
-                CoroutineHandle handle = Timing.RunCoroutine(InternalWaitUntil(script, RawArguments.JoinMessage(1)), coroutineKey);
-                CoroutineHelper.AddCoroutine("WAITUNTIL", handle, script);
-
-                message = new(true);
-                return Timing.WaitUntilDone(handle);
-            }
-
-            if (!ConditionHelper.TryMath(formula, out MathResult result))
-            {
-                message = new(false, "diot");
-                return null;
-            }
-
-            if (result.Result < 0)
-            {
-                message = new(false, "diot");
-                return null;
-            }
-
             message = new(true);
-            return Timing.WaitForSeconds(Arguments[0].ToUpper() == "MIL" ? result.Result / 1000 : result.Result);
-        }
-
-        private IEnumerator<float> InternalWaitUntil(Script script, string input)
-        {
-            while (true)
-            {
-                ConditionResponse response = ConditionHelper.Evaluate(input, script);
-                if (response.Success)
-                {
-                    if (response.Passed)
-                        break;
-                }
-                else
-                {
-                    Logger.ScriptError($"WaitUntil condition error: {response.Message}", script);
-                    break;
-                }
-
-                yield return Timing.WaitForSeconds(1 / MainPlugin.Configs.WaitUntilFrequency);
-            }
+            return Timing.WaitForSeconds((float)((TimeSpan)Arguments[0]!).TotalSeconds);
         }
     }
 }
---
+++ ScriptedEvents/Actions/YieldingActions/WaitUntilAction.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using MEC;
+using ScriptedEvents.API.Extensions;
+using ScriptedEvents.API.Features;
+using ScriptedEvents.Enums;
+using ScriptedEvents.Interfaces;
+using ScriptedEvents.Structures;
+
+namespace ScriptedEvents.Actions.YieldingActions
+{
+    public class WaitUntilAction : ITimingAction, IHelpInfo
+    {
+        /// <inheritdoc/>
+        public string Name => "WaitUntil";
+
+        /// <inheritdoc/>
+        public string[] Aliases => Array.Empty<string>();
+
+        /// <inheritdoc/>
+        public string[] RawArguments { get; set; }
+
+        /// <inheritdoc/>
+        public object?[] Arguments { get; set; }
+
+        /// <inheritdoc/>
+        public ActionSubgroup Subgroup => ActionSubgroup.Yielding;
+
+        /// <inheritdoc/>
+        public string Description => "Yields the script until the provided conditon results in TRUE.";
+
+        /// <inheritdoc/>
+        public Argument[] ExpectedArguments => new[]
+        {
+            new Argument("condition", typeof(string), "The conditon.", true),
+        };
+
+        /// <inheritdoc/>
+        public float? Execute(Script script, out ActionResponse message)
+        {
+            string coroutineKey = $"WAITUNTIL_COROUTINE_{DateTime.Now.Ticks}";
+            CoroutineHandle handle = Timing.RunCoroutine(InternalWaitUntil(script, RawArguments.JoinMessage()), coroutineKey);
+            CoroutineHelper.AddCoroutine("WAITUNTIL", handle, script);
+
+            message = new(true);
+            return Timing.WaitUntilDone(handle);
+        }
+
+        private IEnumerator<float> InternalWaitUntil(Script script, string input)
+        {
+            while (true)
+            {
+                ConditionResponse response = ConditionHelper.Evaluate(input, script);
+                if (response.Success)
+                {
+                    if (response.Passed)
+                        break;
+                }
+                else
+                {
+                    Logger.ScriptError($"WaitUntil condition error: {response.Message}", script);
+                    break;
+                }
+
+                yield return Timing.WaitForSeconds(1 / MainPlugin.Configs.WaitUntilFrequency);
+            }
+        }
+    }
+}
