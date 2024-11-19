fa9adf226a46d592ca2adf09b702eed75f326deb
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2024-11-19 18:27:21 +0100

    Update StrLenAction.cs

--- ScriptedEvents/Actions/Strings/StrLenAction.cs
+++ ScriptedEvents/Actions/Strings/StrLenAction.cs
@@ -1,25 +1,23 @@
-﻿using ScriptedEvents.Enums;
+﻿using System;
+using ScriptedEvents.Enums;
 using ScriptedEvents.Interfaces;
+using ScriptedEvents.Structures;
 
-namespace ScriptedEvents.Actions
+namespace ScriptedEvents.Actions.Strings
 {
-    using System;
-    using ScriptedEvents.Structures;
-
-    /// <inheritdoc/>
     public class StrLenAction : IScriptAction, IHelpInfo, IMimicsVariableAction
     {
         /// <inheritdoc/>
-        public string Name => "STR-LEN";
+        public string Name => "StrLen";
 
         /// <inheritdoc/>
-        public string Description => "Returns the length of the string.";
+        public string Description => "Returns the length of the provided string.";
 
         /// <inheritdoc/>
         public string[] RawArguments { get; set; }
 
         /// <inheritdoc/>
-        public object[] Arguments { get; set; }
+        public object?[] Arguments { get; set; }
 
         /// <inheritdoc/>
         public string[] Aliases => Array.Empty<string>();
@@ -30,13 +28,13 @@ namespace ScriptedEvents.Actions
         /// <inheritdoc/>
         public Argument[] ExpectedArguments => new[]
         {
-            new Argument("string", typeof(string), "The string.", true),
+            new Argument("string", typeof(string), "The subject string.", true),
         };
 
         /// <inheritdoc/>
         public ActionResponse Execute(Script script)
         {
-            return new(true, variablesToRet: new object[] { ((string)Arguments[0]).Length.ToString() });
+            return new(true, new(((string)Arguments[0]!).Length.ToString()));
         }
     }
 }
