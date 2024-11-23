a27c6893bf04eea32f7508cf151021c80293a564
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2024-11-23 16:26:58 +0100

    add MultiArgumentString

---
+++ ScriptedEvents/Structures/MultiArgumentString.cs
@@ -0,0 +1,11 @@
+﻿namespace ScriptedEvents.Structures
+{
+    /// <summary>
+    /// Represents a string argument which will take all text after this argument and join it as a single string.
+    /// Used in actions like broadcast where the message is longer than a single word and there are no addional args
+    /// behind the message.
+    /// </summary>
+    public class MultiArgumentString
+    {
+    }
+}
